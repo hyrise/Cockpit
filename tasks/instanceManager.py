4cd7cd15893d271bf5753b8873b78fcaa4c2dcf7
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2019-11-22 15:02:21 +0100

    Add InstanzeManager Worker

--- tasks/instanceManager.py
+++ tasks/instanceManager.py
@@ -1,9 +1,36 @@
 import pandas as pd
+import psycopg2
+from rq.worker import Worker
+
+DATABASE_HOST = ""
+DATABASE_PORT = 0
+DATABASE_NAME = "postgres"
+DATABASE_USER = "serviceuser"
+DATABASE_PASSWORD = "serviceuser"
+
+connection_pool = []
+
+
+class MyWorker(Worker):
+    def work(self, *args, **kwargs):
+        connection = psycopg2.connect(
+            dbname=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD,
+            host=DATABASE_HOST,
+            port=DATABASE_PORT,
+        )
+        connection.set_session(autocommit=True)
+        connection_pool.append(connection)
+        return super().work(*args, **kwargs)
+
+
+def getConnection():
+    return connection_pool[0]
 
 
 def getStorageDataTask():
-    # TODO initialize worker with a connection
-    conn = 1
+    conn = getConnection()
     meta_segments = pd.io.sql.read_sql_query("SELECT * FROM meta_segments;", conn)
     meta_segments.set_index(
         ["table", "column_name", "chunk_id"], inplace=True, verify_integrity=True
