21e1d31cdbf91ef33ec921189275767853e9b0c5
fabianhe <fabian.heseding@web.de>
2019-11-27 10:50:40 +0100

    Moved cockpit backend

---
+++ cockpit/backend/settings.py
@@ -0,0 +1,20 @@
+"""Module for loading environment variables.
+
+Variables that should not be commited can be stored in a .env file:
+
+```
+FOO=BAR
+EX=AMPLE
+```
+
+Set those variables in the .env file, load them here,
+and import the variables from this module.
+"""
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HI_HOST = getenv("HI_HOST")
+HI_PORT = getenv("HI_PORT")
