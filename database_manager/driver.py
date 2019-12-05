f4c13cf51c05176388abfa81649ae6a302a2e750
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2019-12-05 15:47:01 +0100

    Rework db driver

--- database_manager/driver.py
+++ database_manager/driver.py
@@ -4,23 +4,30 @@ import psycopg2
 
 
 class Driver(object):
-    """Driver for a Databasetype."""
+    """Interface to database."""
 
-    def __init__(self, user, password, host, port, dbname):
+    def __init__(self, user, password, host, port, dbname, number_threads):
         """Initialize the connection."""
-        self.connection = psycopg2.connect(
+        self._user = user
+        self._password = password
+        self._host = host
+        self._port = (port,)
+        self._dbname = dbname
+        self._connection_pool = self._create_connection_pool(number_threads)
+
+    def _create_connection_pool(self, number_threads):
+        """Create thread save connection pool."""
+        connection_pool = psycopg2.pool.ThreadedConnectionPool(
+            0,
+            number_threads,
             user=self._user,
             password=self._password,
             host=self._host,
-            port=self._port,
-            dbname=self._dbname,
+            port="____",  # Add Port
+            database=self._dbname,
         )
-        self.cur = self.connection.cursor()
+        return connection_pool
 
-    def execute(self, query):
-        """Execute a cuery."""
-        self.cur.execute(query)
-
-    def get_storage_data(self):
-        """Get storage data from database."""
-        return "Dummy"
+    def get_connection_pool(self):
+        """Return the connection pool."""
+        return self._connection_pool
