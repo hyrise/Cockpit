a48befbaf2d6e2c2b3bc86bf48e10cf8b492cf1f
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2019-12-06 12:49:29 +0100

    Add subscriber worker for filling task queue

--- database_manager/db_object.py
+++ database_manager/db_object.py
@@ -1,6 +1,26 @@
 """The database object represents the instanz of a database."""
 
+import atexit
+import json
 from multiprocessing import Manager, Process, Queue
+
+import zmq
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def task_fill_task_queue(workload_publisher_url, task_queue):
+    """Fill the queue."""
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect(workload_publisher_url)
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
+
+    while True:
+        content = subscriber.recv_string()
+        data = json.loads(content)
+        querys = data["Content"]
+        for i in range(len(querys)):
+            task_queue.put(querys[i])
 
 
 def task_execute_querys(
@@ -11,7 +31,7 @@ def task_execute_querys(
     connection.set_session(autocommit=True)
     cur = connection.cursor()
     while True:
-        # If Queue is emty go to sleep
+        # If Queue is emty go to wait status
         task = task_queue.get(block=True)
         cur.execute(task)
         throughput_data_container[str(worker_id)] = (
@@ -22,16 +42,25 @@ def task_execute_querys(
 class DbObject(object):
     """Represents database."""
 
-    def __init__(self, number_worker, driver):
-        """Initialize the database object."""
+    def __init__(self, number_worker, driver, workload_publisher_url):
+        """Initialize database object."""
         self.number_worker = number_worker
+        self.workload_publisher_url = workload_publisher_url
+        self._throughput_counter = 0
         self._manager = Manager()
-        self._task_queue = Queue()
+        # Queue has infinite size
+        self._task_queue = Queue(0)
         self._connection_pool = driver.get_connection_pool()
         self._throughput_data_container = self._init_throughput_data_container()
         self._worker_pool = self._init_worler_pool()
         self._start_workers()
         self._lost = 0
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.add_job(
+            func=self.update_throughput_data, trigger="interval", seconds=1,
+        )
+        self.scheduler.start()
+        atexit.register(lambda: self.scheduler.shutdown())
 
     def _init_throughput_data_container(self):
         """Initialize meta data container."""
@@ -54,6 +83,11 @@ class DbObject(object):
                 ],
             )
             worker_pool.append(p)
+        subscriber_process = Process(
+            target=task_fill_task_queue,
+            args=[self.workload_publisher_url, self._task_queue],
+        )
+        worker_pool.append(subscriber_process)
         return worker_pool
 
     def _start_workers(self):
@@ -61,27 +95,21 @@ class DbObject(object):
         for i in range(len(self._worker_pool)):
             self._worker_pool[i].start()
 
-    def execute_workload(self, workload):
-        """Put workload in queue."""
-        # Eventuell ist es sinnvoll für jeden Worker eine Queue anzulegen
-        # Max size ist ca 30.000
-        for i in range(len(workload)):
-            if self._task_queue.full():
-                self._lost = self._lost + 1
-            else:
-                self._task_queue.put(workload[i])
-
     def execute_query(self, query):
         """Put single query in queue."""
         self._task_queue.put(query)
 
-    def get_throughput_data(self):
+    def get_throughput_counter(self):
+        """Return throughput."""
+        return self._throughput_counter
+
+    def update_throughput_data(self):
         """Put meta data from all workers together."""
         throughput_data = 0
         for i in range(self.number_worker):
             throughput_data = throughput_data + self._throughput_data_container[str(i)]
             self._throughput_data_container[str(i)] = 0
-        return throughput_data
+        self._throughput_counter = throughput_data
 
     def _close_pool(self):
         """Close worker pool."""
@@ -92,7 +120,7 @@ class DbObject(object):
     def _close_connections(self):
         """Close connections."""
         self._connection_pool.closeall()
-        print("Pool closed")
+        print("Connectio pool closed")
 
     def _close_queue(self):
         """Close queue."""
@@ -102,6 +130,6 @@ class DbObject(object):
     def clean_exit(self):
         """Clean exit."""
         print(f"lost {self._lost}")
-        self._close_queue()
         self._close_pool()
         self._close_connections()
+        self._close_queue()
