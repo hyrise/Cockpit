e8f25b06ae361574d0d609cd70139216604050c1
Alexander-Dubrawski <alexander.dubrawski@googlemail.com>
2019-12-13 15:52:43 +0000

    Remove throughput dict

--- hyrisecockpit/database_manager/manager.py
+++ hyrisecockpit/database_manager/manager.py
@@ -1,15 +1,10 @@
 """Module for managing databases."""
-from os import getenv
 
 from zmq import REP, Context
 
 import settings as s
 from db_object import DbObject
 from driver import Driver
-
-NUMBER_THREADS = 8
-SUB_URL = getenv("WORKER_SUB_URL")
-REP_URL = getenv("MANAGER_REP_URL")
 
 responses = {
     200: {"header": {"status": 200, "message": "OK"}, "body": {}},
@@ -34,7 +29,7 @@ class DatabaseManager(object):
         self._context = Context(io_threads=1)
         self._socket = self._context.socket(REP)
         self._socket.bind(
-            "tcp://{:s}:{:4d}".format(s.DB_MANAGER_HOST, s.DB_MANAGER_PORT)
+            "tcp://{:s}:{:s}".format(s.DB_MANAGER_HOST, s.DB_MANAGER_PORT)
         )
         self._run()
 
@@ -46,19 +41,23 @@ class DatabaseManager(object):
             host=body["host"],
             port=body["port"],
             dbname=body["dbname"],
-            n_threads=body["n_threads"],
+            n_threads=int(body["n_threads"]),
         )
-        db_instance = DbObject(NUMBER_THREADS, driver, SUB_URL)
+        db_instance = DbObject(
+            int(body["n_threads"]),
+            driver,
+            "tcp://{:s}:{:s}".format(s.WORKLOAD_SUB_HOST, s.WORKLOAD_PUBSUB_PORT),
+        )
         self._drivers[body["id"]] = db_instance
-        self._throughput[body["id"]] = 0
         return responses[200]
 
     def _call_throughput(self, body):
         """Get the throughput of all databases."""
+        throughput = {}
         for database, database_object in self._drivers.items():
-            self._throughput[database] = database_object.get_throughput_counter()
+            throughput[database] = database_object.get_throughput_counter()
         response = responses[200]
-        response["body"]["throughput"] = self._throughput
+        response["body"]["throughput"] = throughput
         return response
 
     def _call_not_found(self, body):
@@ -67,7 +66,7 @@ class DatabaseManager(object):
     def _run(self):
         """Run the manager by enabling IPC."""
         print(
-            "Database manager running on {:s}:{:4d}. Press CTRL+C to quit.".format(
+            "Database manager running on {:s}:{:s}. Press CTRL+C to quit.".format(
                 s.DB_MANAGER_HOST, s.DB_MANAGER_PORT
             )
         )
