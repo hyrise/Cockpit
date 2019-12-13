36280fe3c1bd4486ad128fcc4921e5a52fc1982b
Alexander-Dubrawski <alexander.dubrawski@googlemail.com>
2019-12-13 18:11:02 +0000

    Size of connection pool is now defined by number of connections

--- hyrisecockpit/database_manager/driver.py
+++ hyrisecockpit/database_manager/driver.py
@@ -6,20 +6,20 @@ from psycopg2 import pool
 class Driver(object):
     """Interface to database."""
 
-    def __init__(self, user, password, host, port, dbname, n_threads):
+    def __init__(self, user, password, host, port, dbname, n_connections):
         """Initialize the connection."""
         self._user = user
         self._password = password
         self._host = host
         self._port = port
         self._dbname = dbname
-        self._connection_pool = self._create_connection_pool(n_threads)
+        self._connection_pool = self._create_connection_pool(n_connections)
 
-    def _create_connection_pool(self, n_threads):
+    def _create_connection_pool(self, n_connections):
         """Create thread save connection pool."""
         connection_pool = pool.ThreadedConnectionPool(
             0,
-            n_threads,
+            n_connections,
             user=self._user,
             password=self._password,
             host=self._host,
