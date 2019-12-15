4d6f59207d4bf6fa88782caa84032095916f78ff
Fabian Heseding <39628987+fabianhe@users.noreply.github.com>
2019-12-15 10:00:25 +0100

    Add sample tests (#52)
    * Remove duplicate code from generator
    
    * Fix manager imports, change naming of drivers to databases
    
    * Add init test for workload producer

--- hyrisecockpit/database_manager/manager.py
+++ hyrisecockpit/database_manager/manager.py
@@ -3,9 +3,10 @@ from os import getenv
 
 from zmq import REP, Context
 
-import settings as s
-from db_object import DbObject
-from driver import Driver
+from hyrisecockpit import settings as s
+
+from .db_object import DbObject
+from .driver import Driver
 
 NUMBER_THREADS = 8
 SUB_URL = getenv("WORKER_SUB_URL")
@@ -22,21 +23,19 @@ class DatabaseManager(object):
 
     def __init__(self):
         """Initialize a DatabaseManager."""
-        self._drivers = dict()
+        self._databases = dict()
         self._server_calls = {
             "add database": self._call_add_database,
             "throughput": self._call_throughput,
         }
         self._init_server()
-        self._run()
 
     def _init_server(self):
         self._context = Context(io_threads=1)
         self._socket = self._context.socket(REP)
         self._socket.bind(
-            "tcp://{:s}:{:4d}".format(s.DB_MANAGER_HOST, s.DB_MANAGER_PORT)
+            "tcp://{:s}:{:s}".format(s.DB_MANAGER_HOST, s.DB_MANAGER_PORT)
         )
-        self._run()
 
     def _call_add_database(self, body):
         """Add database and initialize driver for it."""
@@ -49,13 +48,13 @@ class DatabaseManager(object):
             n_threads=body["n_threads"],
         )
         db_instance = DbObject(NUMBER_THREADS, driver, SUB_URL)
-        self._drivers[body["id"]] = db_instance
+        self._databases[body["id"]] = db_instance
         self._throughput[body["id"]] = 0
         return responses[200]
 
     def _call_throughput(self, body):
         """Get the throughput of all databases."""
-        for database, database_object in self._drivers.items():
+        for database, database_object in self._databases.items():
             self._throughput[database] = database_object.get_throughput_counter()
         response = responses[200]
         response["body"]["throughput"] = self._throughput
@@ -64,10 +63,10 @@ class DatabaseManager(object):
     def _call_not_found(self, body):
         return responses[400]
 
-    def _run(self):
-        """Run the manager by enabling IPC."""
+    def start(self):
+        """Start and run the manager by enabling IPC."""
         print(
-            "Database manager running on {:s}:{:4d}. Press CTRL+C to quit.".format(
+            "Database manager running on {:s}:{:s}. Press CTRL+C to quit.".format(
                 s.DB_MANAGER_HOST, s.DB_MANAGER_PORT
             )
         )
@@ -86,7 +85,7 @@ class DatabaseManager(object):
 
 def main():
     """Run a database manager."""
-    DatabaseManager()
+    DatabaseManager().start()
 
 
 if __name__ == "__main__":
