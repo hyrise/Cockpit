dfe7fd14028c9777840950a0f75390bb4b900c31
fabianhe <fabian.heseding@web.de>
2019-12-18 21:25:41 +0100

    Add CPU/RAM dummy data

--- debug/generate_dummy_data.py
+++ debug/generate_dummy_data.py
@@ -6,3 +6,10 @@ with open("influx.csv", "w") as f:
     for id in range(25000):
         tp = randint(10000, 16000)  # nosec
         print(id, tp, sep=",", file=f)
+
+with open("system.csv", "w") as f:
+    print("ID", "CPU", "RAM", sep=",", file=f)
+    for id in range(25000):
+        cpu = randint(0, 100)  # nosec
+        ram = randint(0, 100)  # nosec
+        print(id, cpu, ram, sep=",", file=f)
