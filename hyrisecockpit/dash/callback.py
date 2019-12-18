91648cb33d4dd3454ebc2a35b14bb71675e2a48f
fabianhe <fabian.heseding@web.de>
2019-12-18 21:29:18 +0100

    Add CPU/RAM figure callbacks

--- hyrisecockpit/dash/callback.py
+++ hyrisecockpit/dash/callback.py
@@ -4,7 +4,12 @@ from typing import Dict
 from dash.dependencies import Input, Output
 
 from app import app
-from figures import draw_storage_figure, draw_throughput_figure
+from figures import (
+    draw_cpu_figure,
+    draw_ram_figure,
+    draw_storage_figure,
+    draw_throughput_figure,
+)
 
 
 @app.callback(
@@ -21,3 +26,19 @@ def update_throughput_graph(n_intervals: int) -> Dict:
 def update_storage_graph(n_intervals: int) -> Dict:
     """Update the symbol graph showing candlesticks for the selected symbol."""
     return draw_storage_figure(n_intervals)
+
+
+@app.callback(
+    Output("cpu-graph", "figure"), [Input("throughput-interval", "n_intervals")],
+)
+def update_cpu_graph(n_intervals: int) -> Dict:
+    """Update the symbol graph showing candlesticks for the selected symbol."""
+    return draw_cpu_figure(n_intervals)
+
+
+@app.callback(
+    Output("ram-graph", "figure"), [Input("throughput-interval", "n_intervals")],
+)
+def update_ram_graph(n_intervals: int) -> Dict:
+    """Update the symbol graph showing candlesticks for the selected symbol."""
+    return draw_ram_figure(n_intervals)
