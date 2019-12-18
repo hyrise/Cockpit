aaa357933609214bc612e447171bda3527b0efdd
fabianhe <fabian.heseding@web.de>
2019-12-18 21:28:26 +0100

    Add CPU/RAM figures to layout

--- hyrisecockpit/dash/layout.py
+++ hyrisecockpit/dash/layout.py
@@ -3,38 +3,28 @@ from dash_bootstrap_components import Col, Container, NavbarSimple, Row
 from dash_core_components import Graph, Interval
 from dash_html_components import Div
 
-throughput_graph = Graph(
-    id="throughput-graph",
-    config={
-        "displaylogo": False,
-        "displayModeBar": False,
-        "scrollZoom": False,
-        "editable": False,
-        "staticPlot": True,
-        "doubleClick": False,
-    },
-)
+config = {
+    "displaylogo": False,
+    "displayModeBar": False,
+    "scrollZoom": False,
+    "editable": False,
+    "staticPlot": True,
+    "doubleClick": False,
+}
+throughput_graph = Graph(id="throughput-graph", config=config,)
 
-storage_graph = Graph(
-    id="storage-graph",
-    config={
-        "displaylogo": False,
-        "displayModeBar": False,
-        "scrollZoom": False,
-        "editable": False,
-        "staticPlot": True,
-        "doubleClick": False,
-    },
-)
+storage_graph = Graph(id="storage-graph", config=config,)
+
+cpu_graph = Graph(id="cpu-graph", config=config,)
+ram_graph = Graph(id="ram-graph", config=config,)
 
 body = Container(
     [
         Interval("throughput-interval", interval=1 * 1000, n_intervals=0),
         Interval("storage-interval", interval=60 * 1000, n_intervals=0),
-        Row(Col([throughput_graph])),
-        Row(Col([storage_graph])),
+        Row([Col([cpu_graph], width=3), Col([throughput_graph], width=9)]),
+        Row([Col([ram_graph], width=3), Col([storage_graph], width=9)]),
     ],
-    className="mt-4",
     fluid=True,
 )
 
