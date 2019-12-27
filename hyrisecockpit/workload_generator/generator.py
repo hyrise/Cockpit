14a2c12efc6cdd9eba80a1e1b143375ba5c98fbb
PeterTsayun <petr.tsayun@student.hpi.de>
2019-12-27 11:40:18 +0000

    Single threaded workload generator

--- hyrisecockpit/workload_generator/generator.py
+++ hyrisecockpit/workload_generator/generator.py
@@ -3,21 +3,15 @@
 Includes the main WorkloadGenerator, which uses multiple WorkloadProducers.
 The WorkloadGenerator manages the WorkloadProducers.
 """
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict
 
-from zmq import REP, Context
+from zmq import PUB, REP, Context
 
 from hyrisecockpit.response import get_response
 
-from .producer import WorkloadProducer
-
 
 class WorkloadGenerator(object):
-    """A manager for multiple WorkloadProducers.
-
-    Producers may be added or popped sequentially, or set to a specific number.
-    The WorkloadGenerator may be started or stopped.
-    """
+    """Object responsible for generating workload."""
 
     def __init__(
         self,
@@ -26,30 +20,51 @@ class WorkloadGenerator(object):
         workload_sub_host: str,
         workload_pubsub_port: str,
     ) -> None:
-        """Initialize a WorkloadGenerator with an empty list of WorkloadProducers."""
+        """Initialize a WorkloadGenerator."""
         self._generator_host = generator_host
         self._generator_port = generator_port
         self._workload_sub_host = workload_sub_host
         self._workload_pubsub_port = workload_pubsub_port
         self._server_calls: Dict[str, Callable[[Dict[str, Any]], Dict[str, Any]]] = {
-            "start": self._call_start,
-            "stop": self._call_stop,
+            "workload": self._call_workload,
             "shutdown": self._call_shutdown,
         }
-        self._producers: List[WorkloadProducer] = []
         self._shutdown_requested: bool = False
         self._init_server()
 
     def _init_server(self) -> None:
         self._context = Context(io_threads=1)
-        self._socket = self._context.socket(REP)
-        self._socket.bind(
+        self._rep_socket = self._context.socket(REP)
+        self._rep_socket.bind(
             "tcp://{:s}:{:s}".format(self._generator_host, self._generator_port)
         )
+        self._pub_socket = self._context.socket(PUB)
+        self._pub_socket.bind(
+            "tcp://{:s}:{:s}".format(
+                self._workload_sub_host, self._workload_pubsub_port
+            )
+        )
+
+    def _call_shutdown(self, body: Dict) -> Dict:
+        self._shutdown_requested = True
+        return get_response(200)
+
+    def _call_workload(self, body: Dict) -> Dict:
+        queries = []
+        for _ in range(body["factor"]):
+            queries.append(("SELECT 1;", None))
+        response = get_response(200)
+        response["body"] = {"querylist": queries}
+        self._pub_socket.send_json(response)
+        return response
+
+    def _call_not_found(self, body: Dict) -> Dict:
+        return get_response(400)
 
     def close(self) -> None:
         """Close the socket and context."""
-        self._socket.close()
+        self._rep_socket.close()
+        self._pub_socket.close()
         self._context.term()
 
     def __enter__(self):
@@ -60,57 +75,6 @@ class WorkloadGenerator(object):
         """Call close with a context manager."""
         self.close()
 
-    def _add_producer(self) -> None:
-        """Increase the number of WorkloadProducers by one."""
-        p = WorkloadProducer(
-            f"WorkloadProducer {len(self._producers)}",
-            self._workload_sub_host,
-            self._workload_pubsub_port,
-        )
-        self._producers.append(p)
-        p.start()
-
-    def _pop_producer(self) -> None:
-        """Decrease the number of WorkloadProducers by one."""
-        if self._producers:
-            p = self._producers.pop()
-            p.terminate()
-            p.join()
-
-    def _set_producers(self, number: int) -> int:
-        """Set the number of WorkloadProducers."""
-        delta = number - len(self._producers)
-        if delta < 0:
-            f = self._add_producer
-        elif delta > 0:
-            f = self._pop_producer
-        [f() for _ in range(delta)]
-        return self._get_producers()
-
-    def _get_producers(self) -> int:
-        """Return the number of WorkloadProducers."""
-        return len(self._producers)
-
-    def _call_start(self, body: Dict) -> Dict:
-        """Start generating workloads with n_producers."""
-        n_producers = body["n_producers"]
-        for _ in range(n_producers):
-            self._add_producer()
-        return get_response(200)
-
-    def _call_stop(self, body: Dict) -> Dict:
-        """Stop generating workloads and kill all WorkloadProducers."""
-        for _ in range(len(self._producers)):
-            self._pop_producer()
-        return get_response(200)
-
-    def _call_shutdown(self, body: Dict) -> Dict:
-        self._shutdown_requested = True
-        return get_response(200)
-
-    def _call_not_found(self, body: Dict) -> Dict:
-        return get_response(400)
-
     def start(self) -> None:
         """Run the generator by enabling IPC."""
         print(
@@ -120,7 +84,7 @@ class WorkloadGenerator(object):
         )
         while True:
             # Get the message
-            request = self._socket.recv_json()
+            request = self._rep_socket.recv_json()
 
             # Handle the call
             response = self._server_calls.get(
@@ -128,7 +92,7 @@ class WorkloadGenerator(object):
             )(request["body"])
 
             # Send the reply
-            self._socket.send_json(response)
+            self._rep_socket.send_json(response)
 
             # Shutdown
             if self._shutdown_requested:
