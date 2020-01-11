00428d0c6dcef488eed4ccd729b95ca84648686d
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-01-11 13:17:08 +0000

    Add generic workload reader

---
+++ hyrisecockpit/workload_generator/workloads/workload_reader.py
@@ -0,0 +1,26 @@
+"""Module for reading queries."""
+import os
+from typing import Dict, List
+
+
+class WorkloadReader(object):
+    """Generic workload reader."""
+
+    def append_delimiter(self, queries, delimiter):
+        """Append delimiter back on query."""
+        for query in queries:
+            query = query + delimiter
+
+    def read_from_folder(self, location, delimiter, type) -> Dict[str, List[str]]:
+        """Read sql queries from files in folder."""
+        queries: Dict[str, List[str]] = {}
+        directory = os.fsencode(location)
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(f".{type}"):
+                with open(f"{location}/{filename}", "r") as f:
+                    sub_queries = f.read().split(delimiter)
+                    self.append_delimiter(sub_queries, delimiter)
+                    queries[os.path.splitext(filename)[0]] = sub_queries
+
+        return queries
