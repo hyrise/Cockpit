4478c988f900dc660e21045425dbaddf2d084f02
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-01-11 16:05:36 +0000

    But no-ops workload inside class

--- hyrisecockpit/workload_generator/workloads/noops.py
+++ hyrisecockpit/workload_generator/workloads/noops.py
@@ -3,7 +3,14 @@
 from typing import Any, List, Tuple
 
 
-def generate_noops(factor: int) -> List[Tuple[str, Any]]:
-    """Generate no operational workload."""
-    queries = [("SELECT 1;", None) for _ in range(factor)]
-    return queries
+class NoopsWorkload(object):
+    """Generate no-ops workload."""
+
+    def __init__(self, factor):
+        """Initialize a NoopsWorkload."""
+        self._factor = factor
+
+    def generate_workload(self) -> List[Tuple[str, Any]]:
+        """Generate no operational workload."""
+        queries = [("SELECT 1;", None) for _ in range(self._factor)]
+        return queries
