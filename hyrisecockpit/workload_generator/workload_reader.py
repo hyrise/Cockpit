74616c16541928b86abfd9a7d3a54ff43bbd45fd
PeterTsayun <petr.tsayun@student.hpi.de>
2020-01-13 21:00:09 +0000

    Add WorkloadReader tests

--- hyrisecockpit/workload_generator/workload_reader.py
+++ hyrisecockpit/workload_generator/workload_reader.py
@@ -6,37 +6,51 @@ from typing import Dict, List
 class WorkloadReader(object):
     """Generic workload reader."""
 
-    def append_delimiter(self, queries, delimiter):
-        """Append delimiter back on query."""
+    def _append_delimiter(self, queries, delimiter):
         for i in range(len(queries)):
             queries[i] = queries[i] + delimiter
 
-    def read_from_folder(
-        self, relative_workload_location, delimiter, file_type, workload_type
-    ) -> Dict[str, List[str]]:
-        """Read sql queries from files in folder."""
-        queries: Dict[str, List[str]] = {}
-        absolute_workload_reader_location = os.path.dirname(__file__)
-        absolute_workload_location = (
-            f"{absolute_workload_reader_location}/{relative_workload_location}"
+    def _create_absolute_workload_path(self, relative_workload_path):
+        absolute_workload_reader_path = os.path.dirname(__file__)
+        absolute_workload_path = (
+            f"{absolute_workload_reader_path}/{relative_workload_path}"
         )
-        if not os.path.exists(absolute_workload_location):
+        return absolute_workload_path
+
+    def _get_workload_files(self, absolute_workload_path, workload_type):
+        if not os.path.exists(absolute_workload_path):
             raise Exception(
-                f"Workload {workload_type} not found: directory doesn't exists"
+                f"Workload {workload_type} not found: directory doesn't exist"
             )
-
-        directory = os.fsencode(absolute_workload_location)
-
+        directory = os.fsencode(absolute_workload_path)
         if len(os.listdir(directory)) == 0:
             raise Exception(f"Workload {workload_type} directory is empty")
+        return os.listdir(directory)
 
-        for file in os.listdir(directory):
+    def _read_workload_files(
+        self, workload_files, absolute_workload_path, file_type, delimiter
+    ):
+        queries: Dict[str, List[str]] = {}
+        for file in workload_files:
             filename = os.fsdecode(file)
             if filename.endswith(f".{file_type}"):
-                with open(f"{absolute_workload_location}/{filename}", "r") as f:
+                with open(f"{absolute_workload_path}/{filename}", "r") as f:
                     sub_queries = f.read().split(delimiter)
                     del sub_queries[-1]
-                    self.append_delimiter(sub_queries, delimiter)
+                    self._append_delimiter(sub_queries, delimiter)
                     queries[os.path.splitext(filename)[0]] = sub_queries
-
         return queries
+
+    def read_from_folder(
+        self, relative_workload_path, delimiter, file_type, workload_type
+    ) -> Dict[str, List[str]]:
+        """Read sql queries from files in folder."""
+        absolute_workload_path = self._create_absolute_workload_path(
+            relative_workload_path
+        )
+        workload_files = self._get_workload_files(absolute_workload_path, workload_type)
+        workload = self._read_workload_files(
+            workload_files, absolute_workload_path, file_type, delimiter
+        )
+
+        return workload
--- tests/workload_generator/test_workload_reader.py
+++ tests/workload_generator/test_workload_reader.py
@@ -1,14 +1,47 @@
 """Tests for WorkloadReader."""
-# from unittest import mock
-#
-# from pytest import fixture, mark
-#
-# from hyrisecockpit.workload_generator.workload_reader import WorkloadReader
+from unittest import mock
+
+from pytest import raises
+
+from hyrisecockpit.workload_generator.workload_reader import WorkloadReader
 
 
 class TestWorkloadReader:
     """Tests for the WorkloadReader class."""
 
-    def test_true(self):
-        """Test."""
-        assert True
+    def false_function(self, *argv):
+        """Return False."""
+        return False
+
+    @mock.patch("os.path.exists",)
+    def test_get_workload_files_from_not_existing_directory(self, mock_path_function):
+        """Ensure not existing workload causes exception."""
+        mock_path_function.return_value = False
+        reader = WorkloadReader()
+        with raises(Exception) as e:
+            reader._get_workload_files("absolute_path", "tpch")
+        assert str(e.value) == """Workload tpch not found: directory doesn't exist"""
+
+    @mock.patch("os.path.exists",)
+    @mock.patch("os.listdir",)
+    def test_get_workload_files_from_empty_directory(
+        self, mock_list_function, mock_path_function
+    ):
+        """Ensure empty workload causes exception."""
+        mock_path_function.return_value = True
+        mock_list_function.return_value = []
+        reader = WorkloadReader()
+        with raises(Exception) as e:
+            reader._get_workload_files("absolute_path", "tpch")
+        assert str(e.value) == """Workload tpch directory is empty"""
+
+    @mock.patch("os.path.exists",)
+    @mock.patch("os.listdir",)
+    def test_get_workload_files_from_existing_directory(
+        self, mock_list_function, mock_path_function
+    ):
+        """Ensure reading of existing workload."""
+        mock_path_function.return_value = True
+        mock_list_function.return_value = ["file"]
+        reader = WorkloadReader()
+        assert reader._get_workload_files("absolute_path", "tpch") == ["file"]
