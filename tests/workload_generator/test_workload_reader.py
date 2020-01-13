a45aa83b46b5868d3933c5c3db4be92f64041750
PeterTsayun <petr.tsayun@student.hpi.de>
2020-01-13 20:11:49 +0000

    Refactor structure of the tests

---
+++ tests/workload_generator/test_workload_reader.py
@@ -0,0 +1,14 @@
+"""Tests for WorkloadReader."""
+# from unittest import mock
+#
+# from pytest import fixture, mark
+#
+# from hyrisecockpit.workload_generator.workload_reader import WorkloadReader
+
+
+class TestWorkloadReader:
+    """Tests for the WorkloadReader class."""
+
+    def test_true(self):
+        """Test."""
+        assert True
