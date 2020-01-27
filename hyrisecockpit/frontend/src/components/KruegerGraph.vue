352b43f5a9827c64536146ae0903f172a4d07ba8
Max Schneider <max.schneider@student.hpi.de>
2020-01-27 17:04:53 +0100

    delete typos

--- hyrisecockpit/frontend/src/components/KruegerGraph.vue
+++ hyrisecockpit/frontend/src/components/KruegerGraph.vue
@@ -36,7 +36,7 @@ export default createComponent({
     const { databases } = context.root.$databaseData;
     const { data, checkState } = useKruegerService();
 
-    const chartConfiguration = ["Krueger", "Workloads", "amout of queries"];
+    const chartConfiguration = ["Krueger", "Workloads", "amount of queries"];
 
     onMounted(() => {
       setInterval(checkState, 5000);
