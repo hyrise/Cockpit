67459c98d9a19bc522db8f1332351cfb5a1eb235
Peter Tsayun <petr.tsayun@student.hpi.de>
2020-02-11 11:24:08 +0100

    Revert commit: eaa2bbd

--- hyrisecockpit/workload_generator/workloads/workload.py
+++ hyrisecockpit/workload_generator/workloads/workload.py
@@ -23,7 +23,7 @@ class Workload(object):
         self._delimiter: str = delimiter
         self._file_type: str = file_type
         self._queries: Dict[str, List[str]] = {}
-        self._query_type_pointer = 0
+        self._query_pointer = 0
         self._workload_reader = WorkloadReader()
         self._initialize()
 
@@ -32,23 +32,15 @@ class Workload(object):
         self._queries = self._workload_reader.read_workload(
             self._queries_location, self._delimiter, self._file_type, self.workload_type
         )
-        self._query_pointers = {}
-        for query_type in list(self._queries.keys()):
-            self._query_pointers[query_type] = 0
 
     def generate_workload(self, number_queries: int) -> List[Tuple[str, Any]]:
         """Chose random one query from every type."""
         workload_queries: List[Tuple[str, Any]] = []
         query_types = list(self._queries.keys())
         for _ in range(number_queries):
-            query_type = query_types[self._query_type_pointer]
-            index = self._query_pointers[query_type]
-            workload_queries.append((self._queries[query_type][index], None))
-
-            self._query_pointers[query_type] = (index + 1) % len(
-                self._queries[query_type]
-            )
-            self._query_type_pointer = (self._query_type_pointer + 1) % len(query_types)
+            query_type = query_types[self._query_pointer]
+            workload_queries.append((choice(self._queries[query_type]), None))
+            self._query_pointer = (self._query_pointer + 1) % len(query_types)
 
         return workload_queries
 
