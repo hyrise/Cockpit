a84179a9227b99419de771b8415784c261d55643
schTi <49444254+schTi@users.noreply.github.com>
2020-02-19 14:39:07 +0100

    f/feature/228-improve-metric-comparison-table (#250)
    * fix storage component
    
    * add storage dialog
    
    * add chip component

--- hyrisecockpit/frontend/src/components/metrics/Storage.vue
+++ hyrisecockpit/frontend/src/components/metrics/Storage.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="mx-10 my-10">
+    <v-dialog v-model="showDialog" hide-overlay>
+      <template v-slot:activator="{ on }">
+        <v-btn color="secondary" small right dark v-on="on">
+          <v-icon left>mdi-arrow-expand</v-icon> Open detailed view
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>Storage</v-card-title>
+        <Treemap
+          :graph-id="'1' + graphId || 'storage'"
+          :labels="labels"
+          :parents="parents"
+          :values="sizes"
+          :chart-configuration="chartConfiguration"
+          :autosize="false"
+        />
+        <v-spacer />
+        <v-card-actions>
+          <v-btn block color="primary" @click="showDialog = false">
+            Close detailed view
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <Treemap
-      :graph-id="graphId || 'storage'"
+      :graph-id="'2' + graphId || 'storage'"
       :labels="labels"
       :parents="parents"
       :values="sizes"
@@ -28,6 +52,7 @@ interface Data {
   parents: Ref<string[]>;
   sizes: Ref<number[]>;
   chartConfiguration: Ref<string[]>;
+  showDialog: Ref<boolean>;
 }
 
 export default createComponent({
@@ -38,6 +63,7 @@ export default createComponent({
   props: MetricPropsValidation,
   setup(props: MetricProps, context: SetupContext): Data {
     const data = context.root.$metricController.data[props.metric];
+    const showDialog = ref(false);
 
     const labels = ref<string[]>([]);
     const parents = ref<string[]>([]);
@@ -60,7 +86,7 @@ export default createComponent({
       }
     });
 
-    return { labels, parents, sizes, chartConfiguration };
+    return { labels, parents, sizes, chartConfiguration, showDialog };
   }
 });
 </script>
