a6163b8fb60ab3d6b01bef4a5df46462fdbfa698
Til <til.schniese@student.hpi.de>
2020-02-25 17:46:33 +0100

    simplify selection process for dbs and metrics

--- hyrisecockpit/frontend/src/meta/events.ts
+++ hyrisecockpit/frontend/src/meta/events.ts
@@ -1,11 +1,25 @@
 import { eventBus } from "../plugins/eventBus";
 import { Metric } from "@/types/metrics";
+import { Database } from "@/types/database";
 
 export function useMetricEvents(): {
   emitMetricsChangedEvent: (metrics?: Metric[]) => void;
+  emitSelectedMetricsChangedEvent: (metrics?: Metric[]) => void;
 } {
   function emitMetricsChangedEvent(metrics: Metric[] = []): void {
     eventBus.$emit("METRICS_CHANGED", metrics);
   }
-  return { emitMetricsChangedEvent };
+  function emitSelectedMetricsChangedEvent(metrics: Metric[] = []): void {
+    eventBus.$emit("SELECTED_METRICS_CHANGED", metrics);
+  }
+  return { emitMetricsChangedEvent, emitSelectedMetricsChangedEvent };
+}
+
+export function useDatabaseEvents(): {
+  emitSelectedDatabasesChangedEvent: (databases?: Database[]) => void;
+} {
+  function emitSelectedDatabasesChangedEvent(databases: Database[] = []): void {
+    eventBus.$emit("SELECTED_DATABASES_CHANGED", databases);
+  }
+  return { emitSelectedDatabasesChangedEvent };
 }
--- hyrisecockpit/frontend/src/views/Overview.vue
+++ hyrisecockpit/frontend/src/views/Overview.vue
@@ -1,32 +1,19 @@
 <template>
   <div>
     <div v-if="$databaseService.isReady.value" class="mx-6">
-      <v-select
-        class="select"
-        v-model="watchedInstances"
-        :items="availableInstances"
-        :error="!watchedInstances.length"
-        chips
-        label="databases"
-        multiple
-        clearable
-        deletable-chips
-        return-object
-        outlined
-        prepend-icon="mdi-database"
-      ></v-select>
-      <v-alert v-if="!watchedInstances.length" class="alert" type="warning">
+      <database-metric-selection :metrics="watchedMetrics" />
+      <v-alert v-if="!selectedDatabases.length" class="alert" type="warning">
         No databases selected.
       </v-alert>
       <database-system-details
-        v-if="watchedInstances.length"
-        :databases="watchedInstances.map(database => database.value)"
+        v-if="selectedDatabases.length"
+        :databases="selectedDatabases"
         :handle-scroll="false"
       />
       <MetricsTileList
-        :selected-databases="watchedInstances.map(database => database.value)"
+        :selected-databases="selectedDatabases"
         :show-details="false"
-        :selected-metrics="overviewMetrics"
+        :selected-metrics="selectedMetrics"
       />
     </div>
     <v-progress-linear v-else indeterminate color="primary" height="7" />
@@ -46,32 +33,19 @@ import {
 import MetricsTileList from "../components/container/MetricsTileList.vue";
 import { MetricViewData } from "../types/views";
 import { Metric, overviewMetrics } from "../types/metrics";
-import { useMetricEvents } from "../meta/events";
 import { Database } from "../types/database";
-import { useDatabaseSelection } from "../meta/views";
+import { useSelection } from "../meta/views";
 import DatabaseSystemDetails from "../components/details/DatabaseSystemDetails.vue";
-
-interface Data extends MetricViewData {
-  overviewMetrics: Metric[];
-  availableInstances: Ref<any[]>;
-}
+import DatabaseMetricSelection from "../components/selection/DatabaseMetricSelection.vue";
 
 export default createComponent({
   components: {
     MetricsTileList,
-    DatabaseSystemDetails
+    DatabaseSystemDetails,
+    DatabaseMetricSelection
   },
-  setup(props: {}, context: SetupContext): Data {
-    const { watchedInstances, availableInstances } = useDatabaseSelection(
-      context
-    );
-    const { emitMetricsChangedEvent } = useMetricEvents();
-
-    onMounted(() => {
-      emitMetricsChangedEvent(overviewMetrics);
-    });
-
-    return { watchedInstances, overviewMetrics, availableInstances };
+  setup(props: {}, context: SetupContext): MetricViewData {
+    return { watchedMetrics: overviewMetrics, ...useSelection(context) };
   }
 });
 </script>
