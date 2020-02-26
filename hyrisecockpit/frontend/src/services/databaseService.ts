06cae65a15e0b4551f5ae5acd7220d5af64ff668
Til <til.schniese@student.hpi.de>
2020-02-26 13:40:54 +0100

    add backend connection

--- hyrisecockpit/frontend/src/services/databaseService.ts
+++ hyrisecockpit/frontend/src/services/databaseService.ts
@@ -20,19 +20,19 @@ export function useDatabaseService(): DatabaseService {
 
   function getDatabases(): void {
     axios.get(controlBackend + "database").then(response => {
-      databases.value = response.data.body.databases.map(
-        (databaseId: string) => ({
-          id: databaseId,
-          color: getDatabaseColor(databaseId),
-          systemDetails: {
-            host: databaseId,
-            mainMemoryCapacity: 1000,
-            memoryFootprint: 100,
-            numberOfCPUs: 16,
-            numberOfWorkers: 8
-          }
-        })
-      );
+      databases.value = response.data.map((database: any) => ({
+        id: database.id,
+        color: getDatabaseColor(database.id),
+        systemDetails: {
+          host: database.host,
+          mainMemoryCapacity: 0,
+          memoryFootprint: 0,
+          numberOfCPUs: 0,
+          numberOfWorkers: database.number_workers
+        }
+      }));
+      getDatabaseCPUInformation();
+      getDatabaseMemoryFootprintInformation();
       isReady.value = true;
     });
   }
@@ -49,6 +49,31 @@ export function useDatabaseService(): DatabaseService {
     const color: any = (colors as any)[colorsArray[usedColors]].base;
     usedColors += 2;
     return color;
+  }
+
+  function getDatabaseCPUInformation(): void {
+    axios.get(monitorBackend + "system").then(response => {
+      Object.entries(response.data.body.system_data).forEach(([id, data]) => {
+        const database = databases.value.find(database => database.id === id);
+        database!.systemDetails.numberOfCPUs = Object.keys(
+          (data as any).cpu
+        ).length;
+        database!.systemDetails.mainMemoryCapacity =
+          Math.floor((data as any).memory.total / Math.pow(10, 6)) / 1000;
+      });
+    });
+  }
+
+  function getDatabaseMemoryFootprintInformation(): void {
+    axios.get(monitorBackend + "storage").then(response => {
+      Object.entries(response.data.body.storage).forEach(([id, data]) => {
+        const database = databases.value.find(database => database.id === id);
+        let sum = 0;
+        Object.values(data as any).forEach((table: any) => (sum += table.size));
+        database!.systemDetails.memoryFootprint =
+          Math.floor(sum / Math.pow(10, 3)) / 1000;
+      });
+    });
   }
 
   function addDatabase(databaseService: any): void {
