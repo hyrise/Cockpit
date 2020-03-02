fa8329e75ec663d3144a69a31f32d7ff4da8a229
PeterTsayun <petr.tsayun@student.hpi.de>
2020-03-02 13:56:03 +0000

    Add cursor tests

---
+++ tests/database_manager/test_cursor.py
@@ -0,0 +1,39 @@
+"""Tests for the cursor module."""
+from unittest.mock import MagicMock
+
+from hyrisecockpit.database_manager.cursor import StorageCursor
+
+
+class TestWorker:
+    """Tests Cursor classes."""
+
+    def test_log_queries(self):
+        """Test queries logging."""
+        query_list = [
+            (1, 2, "benchmark1", "query_no_1"),
+            (3, 4, "benchmark2", "query_no_2"),
+        ]
+
+        expected_points = [
+            {
+                "measurement": "successful_queries",
+                "tags": {"benchmark": "benchmark1", "query_no": "query_no_1"},
+                "fields": {"latency": 2},
+                "time": 1,
+            },
+            {
+                "measurement": "successful_queries",
+                "tags": {"benchmark": "benchmark2", "query_no": "query_no_2"},
+                "fields": {"latency": 4},
+                "time": 3,
+            },
+        ]
+
+        cursor = StorageCursor("host", "port", "user", "password", "database")
+        cursor._connection = MagicMock()
+        cursor._connection.write_points.return_value = None
+        cursor.log_queries(query_list)
+
+        cursor._connection.write_points.assert_any_call(
+            expected_points, database="database"
+        )
