d2876b8d01d85f46dae1f3ed75dbe5fc914b4c01
PeterTsayun <petr.tsayun@student.hpi.de>
2020-03-02 12:03:54 +0000

    Adjust and refactor worker functions

--- hyrisecockpit/database_manager/worker.py
+++ hyrisecockpit/database_manager/worker.py
@@ -4,10 +4,11 @@ Workers run in pools and are started by other components.
 """
 from multiprocessing import Queue, Value
 from time import time_ns
+from typing import List, Tuple
 
 from psycopg2 import Error, pool
 from psycopg2.extensions import AsIs
-from zmq import SUB, SUBSCRIBE, Context
+from zmq import SUB, SUBSCRIBE, Context, Socket
 
 from hyrisecockpit.settings import (
     STORAGE_HOST,
@@ -23,16 +24,27 @@ def fill_queue(
     workload_publisher_url: str, task_queue: Queue, processing_tables_flag: Value
 ) -> None:
     """Fill the queue."""
-    context = Context()
-    subscriber = context.socket(SUB)
-    subscriber.connect(workload_publisher_url)
-    subscriber.setsockopt_string(SUBSCRIBE, "")
+    sub_socket = initialize_sub_socket(workload_publisher_url)
     while True:
-        content = subscriber.recv_json()
-        tasks = content["body"]["querylist"]
-        if not processing_tables_flag.value:
-            for task in tasks:
-                task_queue.put(task)
+        fill_task_queue(sub_socket, task_queue, processing_tables_flag)
+
+
+def initialize_sub_socket(publisher_url) -> Socket:
+    """Initialize a socket for subscriber worker."""
+    context = Context()
+    sub_socket = context.socket(SUB)
+    sub_socket.connect(publisher_url)
+    sub_socket.setsockopt_string(SUBSCRIBE, "")
+    return sub_socket
+
+
+def fill_task_queue(sub_socket, task_queue, processing_tables_flag) -> None:
+    """Fill task queue."""
+    content = sub_socket.recv_json()
+    tasks = content["body"]["querylist"]
+    if not processing_tables_flag.value:
+        for task in tasks:
+            task_queue.put(task)
 
 
 def execute_queries(
@@ -52,7 +64,7 @@ def execute_queries(
         with StorageCursor(
             STORAGE_HOST, STORAGE_PORT, STORAGE_USER, STORAGE_PASSWORD, database_id
         ) as log:
-            succesful_queries = []
+            succesful_queries: List[Tuple[int, int, str, str]] = []
             last_batched = time_ns()
             while True:
                 # If Queue is emty go to wait status
@@ -63,27 +75,35 @@ def execute_queries(
                             task_queue.put("wake_up_signal_for_worker")
                         break
                     else:
-                        query_tuple, workload_type, query_type = task
-                        query, not_formatted_parameters = query_tuple
-                        formatted_parameters = (
-                            tuple(
-                                AsIs(parameter) if protocol == "as_is" else parameter
-                                for parameter, protocol in not_formatted_parameters
-                            )
-                            if not_formatted_parameters is not None
-                            else None
-                        )
-                        startts = time_ns()
-                        cur.execute(query, formatted_parameters)
-                        endts = time_ns()
-                        succesful_queries.append(
-                            (endts, endts - startts, workload_type, query_type)
-                        )
+                        handle_task(task, cur, succesful_queries)
                         if last_batched < time_ns() - 1_000_000_000:
                             last_batched = time_ns()
                             log.log_queries(succesful_queries)
                             succesful_queries = []
                 except (ValueError, Error) as e:
                     failed_task_queue.put(
-                        {"worker_id": worker_id, "task": query_tuple, "Error": str(e)}
+                        {"worker_id": worker_id, "task": task, "Error": str(e)}
                     )
+
+
+def handle_task(task, cursor, succesful_queries):
+    """Handle given task."""
+    query, not_formatted_parameters, workload_type, query_type = task
+    formatted_parameters = get_formatted_parameters(not_formatted_parameters)
+
+    startts = time_ns()
+    cursor.execute(query, formatted_parameters)
+    endts = time_ns()
+    succesful_queries.append((endts, endts - startts, workload_type, query_type))
+
+
+def get_formatted_parameters(not_formatted_parameters):
+    """Create formatted parameters."""
+    return (
+        tuple(
+            AsIs(parameter) if protocol == "as_is" else parameter
+            for parameter, protocol in not_formatted_parameters
+        )
+        if not_formatted_parameters is not None
+        else None
+    )
