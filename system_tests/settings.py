c738777248ee2c12615bc418287ba4614cbc98c9
PeterTsayun <petr.tsayun@student.hpi.de>
2020-03-04 12:04:53 +0000

    Add settings

---
+++ system_tests/settings.py
@@ -0,0 +1,29 @@
+"""Module for environmental variables."""
+from os import getenv
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+BACKEND_HOST: str = getenv("BACKEND_HOST", "127.0.0.1")
+BACKEND_PORT: str = getenv("BACKEND_PORT", "20000")
+
+DB_MANAGER_HOST: str = getenv("DB_MANAGER_HOST", "127.0.0.1")
+DB_MANAGER_PORT: str = getenv("DB_MANAGER_PORT", "20001")
+
+GENERATOR_HOST: str = getenv("GENERATOR_HOST", "127.0.0.1")
+GENERATOR_PORT: str = getenv("GENERATOR_PORT", "20002")
+
+WORKLOAD_SUB_HOST: str = getenv("WORKLOAD_SUB_HOST", "127.0.0.1")
+WORKLOAD_PUBSUB_PORT: str = getenv("WORKLOAD_PUBSUB_PORT", "20003")
+
+WORKLOAD_LOCATION: str = getenv("WORKLOAD_LOCATION", "workloads/workload_queries")
+DEFAULT_TABLES: str = getenv("DEFAULT_TABLES", "tpch_0.1")
+
+STORAGE_HOST: str = getenv("STORAGE_HOST", "influxdb")
+STORAGE_PORT: str = getenv("STORAGE_PORT", "8086")
+STORAGE_USER: str = getenv("STORAGE_USER", "root")
+STORAGE_PASSWORD: str = getenv("STORAGE_PASSWORD", "root")
+
+DATABASE1_HOST: str = getenv("DATABASE1_HOST", "vm-york.eaalab.hpi.uni-potsdam.de")
+DATABASE2_HOST: str = getenv("DATABASE2_HOST", "vm-citadelle.eaalab.hpi.uni-potsdam.de")
