3d84332321b89e7e4c6c94c7b375fd2f37866a16
PeterTsayun <petr.tsayun@student.hpi.de>
2020-03-06 13:06:17 +0000

    Disable request logging in Flask

--- system_tests/cli_backend.py
+++ system_tests/cli_backend.py
@@ -1,10 +1,15 @@
 """CLI used to start the backend API."""
+import logging
+
 from hyrisecockpit.backend.app import app
 from settings import BACKEND_HOST, BACKEND_PORT
 
 
 def main() -> None:
     """Create and start a backend API."""
+    log = logging.getLogger("werkzeug")
+    log.disabled = True
+    app.logger.disabled = True
     app.run(host=BACKEND_HOST, port=BACKEND_PORT, threaded=False)
 
 
