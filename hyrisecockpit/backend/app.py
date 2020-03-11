5947c0a0c2f8b16f2050ca2fa480188f1796df79
fabianhe <fabian.heseding@web.de>
2020-03-11 15:46:50 +0100

    Fix loop

--- hyrisecockpit/backend/app.py
+++ hyrisecockpit/backend/app.py
@@ -665,15 +665,21 @@ class AccessData(Resource):
                 database=database,
                 bind_params={"startts": currentts, "endts": endts},
             )
-            for table_name, column_name in list(result.keys()):
-                accesses = {
-                    "table_name": table_name,
-                    "column_name": column_name,
-                    "access_counter": list(result[table_name, column_name])[0][
-                        "access_counter"
+            access_data.append(
+                {
+                    "id": database,
+                    "access_data": [
+                        {
+                            "table_name": table_name,
+                            "column_name": column_name,
+                            "access_counter": list(result[table_name, column_name])[0][
+                                "access_counter"
+                            ],
+                        }
+                        for table_name, column_name in list(result.keys())
                     ],
                 }
-            access_data.append({"id": database, "access_data": accesses})
+            )
         return access_data
 
 
