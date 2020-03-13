2bc8d47bf05029f75bc9b2aec81ee7dfcbc7219c
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2020-03-13 17:20:38 +0100

    make drawer always visible

--- hyrisecockpit/frontend/src/App.vue
+++ hyrisecockpit/frontend/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes['light'].background }">
-    <AppDrawer :open="showNavigationDrawer" />
+    <AppDrawer />
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click="toggleNavigationDrawer()">
-      </v-app-bar-nav-icon>
       <b> Hyrise Cockpit </b>
       <add-database />
     </v-app-bar>
@@ -19,19 +17,12 @@ import AppDrawer from "./views/AppDrawer.vue";
 import addDatabase from "./meta/addDatabase.vue";
 
 interface Data {
-  toggleNavigationDrawer: () => void;
-  showNavigationDrawer: Ref<boolean>;
 }
 
 export default defineComponent({
   components: { AppDrawer, addDatabase },
   setup(props: {}, context: SetupContext): Data {
-    const showNavigationDrawer = ref<boolean>(true);
-
-    function toggleNavigationDrawer(): void {
-      showNavigationDrawer.value = !showNavigationDrawer.value;
-    }
-    return { showNavigationDrawer, toggleNavigationDrawer };
+    return { };
   }
 });
 </script>
