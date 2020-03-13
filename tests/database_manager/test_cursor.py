1389b6eee9cba1a532eb5f25121eee3783508e25
Fabian Heseding <39628987+fabianhe@users.noreply.github.com>
2020-03-13 15:56:39 +0100

    B/more cursor tests (#333)
    * Add log_meta_information test
    
    * Change log_queries test
    
    * Remove point
    
    * Add more fields to test parameters

--- tests/database_manager/test_cursor.py
+++ tests/database_manager/test_cursor.py
@@ -1,5 +1,8 @@
 """Tests for the cursor module."""
+from typing import Any, Dict, List, Tuple
 from unittest.mock import MagicMock
+
+from pytest import mark
 
 from hyrisecockpit.database_manager.cursor import StorageCursor
 
@@ -7,41 +10,63 @@ from hyrisecockpit.database_manager.cursor import StorageCursor
 class TestCursor:
     """Tests Cursor classes."""
 
-    def test_logs_queries(self):
+    @mark.parametrize(
+        "queries",
+        [
+            [(1, 2, "benchmark1", "query_no_1", "worker1")],
+            [
+                (1, 2, "benchmark1", "query_no_1", "worker1"),
+                (3, 4, "benchmark2", "query_no_2", "worker2"),
+            ],
+        ],
+    )
+    def test_logs_queries(self, queries: List[Tuple[int, int, str, str, str]]):
         """Test queries logging."""
-        query_list = [
-            (1, 2, "benchmark1", "query_no_1", "worker1"),
-            (3, 4, "benchmark2", "query_no_2", "worker2"),
-        ]
-
         expected_points = [
             {
                 "measurement": "successful_queries",
                 "tags": {
-                    "benchmark": "benchmark1",
-                    "query_no": "query_no_1",
-                    "worker_id": "worker1",
+                    "benchmark": query[2],
+                    "query_no": query[3],
+                    "worker_id": query[4],
                 },
-                "fields": {"latency": 2},
-                "time": 1,
-            },
-            {
-                "measurement": "successful_queries",
-                "tags": {
-                    "benchmark": "benchmark2",
-                    "query_no": "query_no_2",
-                    "worker_id": "worker2",
-                },
-                "fields": {"latency": 4},
-                "time": 3,
-            },
+                "fields": {"latency": query[1]},
+                "time": query[0],
+            }
+            for query in queries
         ]
 
         cursor = StorageCursor("host", "port", "user", "password", "database")
         cursor._connection = MagicMock()
         cursor._connection.write_points.return_value = None
-        cursor.log_queries(query_list)
+        cursor.log_queries(queries)
 
         cursor._connection.write_points.assert_called_once_with(
             expected_points, database="database"
+        )
+
+    @mark.parametrize(
+        "measurement", ["storage_something", "some_chunks"],
+    )
+    @mark.parametrize(
+        "fields", [{"field1": "some_value"}, {"field1": "some_value", "field2": 9000}],
+    )
+    @mark.parametrize(
+        "time_stamp", [123, 456789],
+    )
+    def test_logs_plugin_log(
+        self, measurement: str, fields: Dict[str, Any], time_stamp: int
+    ):
+        """Test meta_information logging."""
+        expected_point = {
+            "measurement": measurement,
+            "fields": fields,
+            "time": time_stamp,
+        }
+        cursor = StorageCursor("host", "port", "user", "password", "database")
+        cursor._connection = MagicMock()
+        cursor._connection.write_points.return_value = None
+        cursor.log_meta_information(measurement, fields, time_stamp)
+        cursor._connection.write_point.assert_called_once_with(
+            expected_point, database="database"
         )
