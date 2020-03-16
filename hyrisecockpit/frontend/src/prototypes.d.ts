25c811ef2be40f7963c29e5c466e13778b4f0875
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2020-03-16 13:12:22 +0100

    add type to prototypes

--- hyrisecockpit/frontend/src/prototypes.d.ts
+++ hyrisecockpit/frontend/src/prototypes.d.ts
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import { DatabaseController } from "./types/database";
 import { MetricController } from "./types/metrics";
+import { PluginService } from "./types/services";
 
 declare module "vue/types/vue" {
   interface Vue {
     $databaseController: DatabaseController;
     $metricController: MetricController;
-    $pluginService: any;
+    $pluginService: PluginService;
   }
 }
