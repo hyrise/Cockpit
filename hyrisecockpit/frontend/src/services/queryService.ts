5c783308088be025187bc77e56b3d52f4f192ff2
schTi <49444254+schTi@users.noreply.github.com>
2020-03-17 19:48:45 +0100

    f/292-add-query-table (#315)
    * add setup for query table
    
    * fix chip, db controller
    
    * add backend connection
    
    * fix endpoint
    
    * move and simplify
    
    * fix issues
    
    Co-authored-by: Max Schneider <38442947+MaxSchneider1337@users.noreply.github.com>

---
+++ hyrisecockpit/frontend/src/services/queryService.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { monitorBackend } from "../../config";
+import { DetailedQueryInformation } from "@/types/queries";
+
+export function useQueryService(): {
+  getDetailedQueryInformation: () => Promise<
+    Record<string, DetailedQueryInformation[]>
+  >;
+} {
+  async function getDetailedQueryInformation(): Promise<
+    Record<string, DetailedQueryInformation[]>
+  > {
+    let detailedQueryInformation: Record<
+      string,
+      DetailedQueryInformation[]
+    > = {};
+    await axios
+      .get(`${monitorBackend}detailed_query_information`)
+      .then((response: any) => {
+        detailedQueryInformation = response.data.reduce(
+          (
+            queryInformation: Record<string, DetailedQueryInformation[]>,
+            entry: any
+          ) => {
+            const database: string = entry.id;
+            queryInformation[database] = entry.query_information.map(
+              (query: any) => {
+                return {
+                  queryName: query.benchmark + "-" + query.query_number,
+                  workloadType: query.benchmark,
+                  latency: query.latency,
+                  throughput: query.throughput
+                };
+              }
+            );
+            return queryInformation;
+          },
+          {} as Record<string, DetailedQueryInformation[]>
+        );
+      });
+
+    return detailedQueryInformation;
+  }
+
+  return { getDetailedQueryInformation };
+}
