a7cd17550c1b39c9a371754206c8821397f4e1e1
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-03-19 16:03:28 +0000

    Add more tests

--- tests/database_manager/test_worker_pool.py
+++ tests/database_manager/test_worker_pool.py
@@ -4,6 +4,7 @@ from multiprocessing.context import Process as ProcessType
 from multiprocessing.queues import Queue as QueueType
 from multiprocessing.sharedctypes import Synchronized as ValueType
 from multiprocessing.synchronize import Event as EventType
+from typing import Dict
 from unittest.mock import MagicMock, patch
 
 from pytest import fixture
@@ -46,13 +47,30 @@ class TestWorkerPool(object):
         execute_task_worker_done_event,
         worker_continue_event,
     ) -> str:
-        """Return a dummy worker."""
+        """Return a dummy execute task worker."""
+        return "foo"
+
+    def get_fake_dummy_fill_worker(
+        workload_publisher_url,  # noqa
+        task_queue,
+        continue_execution_flag,
+        worker_continue_event,
+    ) -> str:
+        """Return a dummy fill worker."""
         return "foo"
 
     @fixture
     @patch(
         "hyrisecockpit.database_manager.worker_pool.BackgroundScheduler",
         get_fake_background_scheduler,
+    )
+    @patch(
+        "hyrisecockpit.database_manager.worker_pool.execute_queries",
+        get_fake_dummy_execute_queries_worker,
+    )
+    @patch(
+        "hyrisecockpit.database_manager.worker_pool.execute_queries",
+        get_fake_dummy_fill_worker,
     )
     def worker_pool(self) -> WorkerPool:
         """Get a new WorkerPool."""
@@ -99,3 +117,98 @@ class TestWorkerPool(object):
         processes = worker_pool._generate_execute_task_worker()
         assert len(processes) == self.get_number_worker()
         assert all(type(process) is ProcessType for process in processes)
+
+    @patch(
+        "hyrisecockpit.database_manager.worker_pool.execute_queries",
+        get_fake_dummy_fill_worker,
+    )
+    def test_generation_of_fill_task_worker(self, worker_pool) -> None:
+        """Check if enough processes of type process are generated."""
+        process = worker_pool._generate_fill_task_worker()
+        assert type(process) is ProcessType
+
+    def mock_suurounding_init_worker_functions(self, worker_pool) -> None:
+        """Helper-function for overwrite methods."""
+        worker_pool._generate_execute_task_worker_done_events = lambda: ["new_event"]
+        worker_pool._generate_execute_task_worker = lambda: ["new_worker"]
+        worker_pool._generate_fill_task_worker = lambda: "new_fill_worker"
+
+    def get_worker_and_event(self, worker_pool) -> Dict:
+        """Helper-function for returning results."""
+        return {
+            "event": worker_pool._execute_task_worker_done_event,
+            "execute_workers": worker_pool._execute_task_workers,
+            "fill_worker": worker_pool._fill_task_worker,
+        }
+
+    def test_inintialization_of_worker_with_existing_execute_task_worker_and_fill_task_worker(
+        self, worker_pool
+    ) -> None:
+        """Check if nothing get changed while both types of worker exists."""
+        self.mock_suurounding_init_worker_functions(worker_pool)
+
+        worker_pool._execute_task_workers = ["current_worker"]
+        worker_pool._fill_task_worker = "current_fill_worker"
+        worker_pool._execute_task_worker_done_event = ["current_event"]
+
+        worker_pool._init_workers()
+
+        results = self.get_worker_and_event(worker_pool)
+
+        assert results["event"][:] == ["current_event"][:]
+        assert results["execute_workers"][:] == ["current_worker"][:]
+        assert results["fill_worker"] == "current_fill_worker"
+
+    def test_inintialization_of_worker_with_existing_execute_task_worker_and_no_task_worker(
+        self, worker_pool
+    ) -> None:
+        """Check if that new task fill worker is created."""
+        self.mock_suurounding_init_worker_functions(worker_pool)
+
+        worker_pool._execute_task_workers = ["current_worker"]
+        worker_pool._fill_task_worker = None
+        worker_pool._execute_task_worker_done_event = ["current_event"]
+
+        worker_pool._init_workers()
+
+        results = self.get_worker_and_event(worker_pool)
+
+        assert results["event"][:] == ["current_event"][:]
+        assert results["execute_workers"][:] == ["current_worker"][:]
+        assert results["fill_worker"] == "new_fill_worker"
+
+    def test_inintialization_of_worker_with_no_existing_execute_task_worker_and_no_task_worker(
+        self, worker_pool
+    ) -> None:
+        """Check if that new task fill worker and execute task worker are created."""
+        self.mock_suurounding_init_worker_functions(worker_pool)
+
+        worker_pool._execute_task_workers = []
+        worker_pool._fill_task_worker = None
+        worker_pool._execute_task_worker_done_event = ["current_event"]
+
+        worker_pool._init_workers()
+
+        results = self.get_worker_and_event(worker_pool)
+
+        assert results["event"][:] == ["new_event"][:]
+        assert results["execute_workers"][:] == ["new_worker"][:]
+        assert results["fill_worker"] == "new_fill_worker"
+
+    def test_inintialization_of_worker_with_no_existing_execute_task_worker_and_task_worker(
+        self, worker_pool
+    ) -> None:
+        """Check if that new execute task worker are created."""
+        self.mock_suurounding_init_worker_functions(worker_pool)
+
+        worker_pool._execute_task_workers = []
+        worker_pool._fill_task_worker = "current_fill_worker"
+        worker_pool._execute_task_worker_done_event = ["current_event"]
+
+        worker_pool._init_workers()
+
+        results = self.get_worker_and_event(worker_pool)
+
+        assert results["event"][:] == ["new_event"][:]
+        assert results["execute_workers"][:] == ["new_worker"][:]
+        assert results["fill_worker"] == "current_fill_worker"
