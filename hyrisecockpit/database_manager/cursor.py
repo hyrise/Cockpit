28333bad374113a35c8eebbc07fcb87234149ce3
fabianhe <fabian.heseding@web.de>
2020-03-19 11:28:57 +0100

    Add log_failed_queries to cursor

--- hyrisecockpit/database_manager/cursor.py
+++ hyrisecockpit/database_manager/cursor.py
@@ -1,9 +1,23 @@
 """Utility custom cursors."""
 from types import TracebackType
-from typing import Any, Dict, List, Optional, Tuple, Type
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Type, TypedDict
 
 from influxdb import InfluxDBClient
 from psycopg2 import pool
+
+
+class PointBase(TypedDict):
+    """Minimal type of an Influx point for write_points."""
+
+    measurement: str
+    fields: Dict[str, Any]
+
+
+class Point(PointBase, total=False):
+    """Type of an Influx point for write_points."""
+
+    tags: Dict[str, Any]
+    time: int
 
 
 class PoolCursor:
@@ -51,11 +65,11 @@ class StorageCursor:
         self, host: str, port: str, user: str, password: str, database: str
     ) -> None:
         """Initialize a StorageCursor."""
-        self._host = host
-        self._port = port
-        self._user = user
-        self._password = password
-        self._database = database
+        self._host: str = host
+        self._port: str = port
+        self._user: str = user
+        self._password: str = password
+        self._database: str = database
 
     def __enter__(self) -> "StorageCursor":
         """Establish a connection."""
@@ -75,39 +89,58 @@ class StorageCursor:
         self._connection.close()
         return None
 
+    def __write_points(self, points: Iterable[Point]) -> None:
+        """Write multiple points to the database."""
+        return self._connection.write_points(list(points), database=self._database)
+
+    def __write_point(self, point: Point) -> None:
+        """Write a single point to the database."""
+        return self.__write_points([point])
+
     def log_meta_information(
         self, measurement: str, fields: Dict[str, Any], time_stamp: int
     ) -> None:
         """Log meta information in table."""
-        points = [{"measurement": measurement, "fields": fields, "time": time_stamp}]
-        self._connection.write_points(points, database=self._database)
+        self.__write_point(
+            Point(measurement=measurement, fields=fields, time=time_stamp)
+        )
 
     def log_queries(self, query_list: List[Tuple[int, int, str, str, str]]) -> None:
         """Log a couple of succesfully executed queries."""
-        points = [
-            {
-                "measurement": "successful_queries",
-                "tags": {
+        self.__write_points(
+            Point(
+                measurement="successful_queries",
+                tags={
                     "benchmark": query[2],
                     "query_no": query[3],
                     "worker_id": query[4],
                 },
-                "fields": {"latency": query[1]},
-                "time": query[0],
-            }
+                fields={"latency": query[1]},
+                time=query[0],
+            )
             for query in query_list
-        ]
-        self._connection.write_points(points, database=self._database)
+        )
+
+    def log_failed_queries(self, query_list: List[Tuple[int, str, str, str]]):
+        """Log failed queries."""
+        self.__write_points(
+            Point(
+                measurement="failed_queries",
+                tags={"worker_id": query[1]},
+                fields={"task": query[2], "error": query[3]},
+                time=query[0],
+            )
+            for query in query_list
+        )
 
     def log_plugin_log(self, plugin_log: List[Tuple[int, str, str]]) -> None:
         """Log a couple of succesfully executed queries."""
-        points = [
-            {
-                "measurement": "plugin_log",
-                "tags": {"timestamp": row[0], "reporter": row[1]},
-                "fields": {"message": row[2]},
-                "time": row[0],
-            }
+        self.__write_points(
+            Point(
+                measurement="plugin_log",
+                tags={"timestamp": row[0], "reporter": row[1]},
+                fields={"message": row[2]},
+                time=row[0],
+            )
             for row in plugin_log
-        ]
-        self._connection.write_points(points, database=self._database)
+        )
