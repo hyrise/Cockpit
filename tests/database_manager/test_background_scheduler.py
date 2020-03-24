896c8c87feb451437856debd35121f41056b4585
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-03-24 14:21:17 +0000

    Add more tests
    Co-authored-by: caterinamandel98 <38725641+caterinamandel98@users.noreply.github.com>

--- tests/database_manager/test_background_scheduler.py
+++ tests/database_manager/test_background_scheduler.py
@@ -1,6 +1,7 @@
 """Tests for the background_scheduler module."""
 
 from multiprocessing import Value
+from typing import Dict, Optional
 from unittest.mock import MagicMock, patch
 
 from pytest import fixture
@@ -8,7 +9,7 @@ from pytest import fixture
 from hyrisecockpit.database_manager.background_scheduler import BackgroundJobManager
 
 
-class TextBackgroundJobManager:
+class TestBackgroundScheduler:
     """Tests BackgroundJobManager class."""
 
     def get_database_id(self) -> str:
@@ -39,6 +40,19 @@ class TextBackgroundJobManager:
         """Return mocked storage user."""
         return "user?"
 
+    def get_fake_loaded_tables(self) -> Dict[str, Optional[str]]:
+        """Return mocked loaded tables."""
+        fake_loaded_tables: Dict[str, Optional[str]] = {
+            "The Dough Rollers": "alternative",
+            "Broken Witt Rebels": "alternative",
+            "Bonny Doon": "alternative",
+            "Jack White": "alternative",
+            "Gary Clark Jr.": "Rock",
+            "Greta Van Fleet": "Rock",
+            "Tenacious D": "Rock",
+        }
+        return fake_loaded_tables
+
     @fixture
     @patch(
         "hyrisecockpit.database_manager.background_scheduler.BackgroundScheduler",
@@ -54,6 +68,7 @@ class TextBackgroundJobManager:
             self.get_database_id(),
             self.get_database_blocked(),
             self.get_connection_pool(),
+            self.get_fake_loaded_tables(),
             self.get_storage_host(),
             self.get_storage_password(),
             self.get_storage_port(),
@@ -63,3 +78,89 @@ class TextBackgroundJobManager:
     def test_initializes(self, background_job_manager: BackgroundJobManager) -> None:
         """A BackgroundJobManager initializes."""
         assert isinstance(background_job_manager, BackgroundJobManager)
+        assert background_job_manager._database_id == self.get_database_id()
+        assert (
+            background_job_manager._database_blocked.value
+            == self.get_database_blocked().value
+        )
+        assert background_job_manager._storage_host == self.get_storage_host()
+        assert background_job_manager._storage_port == self.get_storage_port()
+        assert background_job_manager._storage_user == self.get_storage_user()
+        assert background_job_manager._storage_password == self.get_storage_password()
+        assert background_job_manager._loaded_tables == self.get_fake_loaded_tables()
+        assert type(background_job_manager._previous_chunks_data) is dict
+
+    def test_initializes_background_scheduler_job(
+        self, background_job_manager: BackgroundJobManager
+    ) -> None:
+        """Test initialization of background scheduler job."""
+        mock_scheduler = MagicMock()
+        mock_scheduler.add_job.return_value = None
+        background_job_manager._scheduler = mock_scheduler
+        background_job_manager._update_krueger_data = MagicMock()  # type: ignore
+        background_job_manager._update_chunks_data = MagicMock()  # type: ignore
+        background_job_manager._update_system_data = MagicMock()  # type: ignore
+        background_job_manager._update_storage_data = MagicMock()  # type: ignore
+        background_job_manager._update_plugin_log = MagicMock()  # type: ignore
+
+        background_job_manager._init_jobs()
+
+        mock_scheduler.add_job.assert_any_call(
+            func=background_job_manager._update_krueger_data,
+            trigger="interval",
+            seconds=5,
+        )
+        mock_scheduler.add_job.assert_any_call(
+            func=background_job_manager._update_chunks_data,
+            trigger="interval",
+            seconds=5,
+        )
+        mock_scheduler.add_job.assert_any_call(
+            func=background_job_manager._update_system_data,
+            trigger="interval",
+            seconds=1,
+        )
+        mock_scheduler.add_job.assert_any_call(
+            func=background_job_manager._update_storage_data,
+            trigger="interval",
+            seconds=5,
+        )
+        mock_scheduler.add_job.assert_any_call(
+            func=background_job_manager._update_plugin_log,
+            trigger="interval",
+            seconds=1,
+        )
+
+    def test_background_scheduler_starts(
+        self, background_job_manager: BackgroundJobManager
+    ) -> None:
+        """Test start of background scheduler."""
+        mock_scheduler = MagicMock()
+        mock_scheduler.start.return_value = None
+        background_job_manager._scheduler = mock_scheduler
+
+        background_job_manager.start()
+
+        mock_scheduler.start.assert_called()
+
+    def test_background_scheduler_closes(
+        self, background_job_manager: BackgroundJobManager
+    ) -> None:
+        """Test clsoe of bachground scheduler object."""
+        mock_scheduler = MagicMock()
+        mock_scheduler.shutdown.return_value = None
+        background_job_manager._scheduler = mock_scheduler
+        background_job_manager._update_krueger_data_job = MagicMock()
+        background_job_manager._update_system_data_job = MagicMock()
+        background_job_manager._update_chunks_data_job = MagicMock()
+        background_job_manager._update_storage_data_job = MagicMock()
+        background_job_manager._update_plugin_log_job = MagicMock()
+
+        background_job_manager.close()
+
+        background_job_manager._update_krueger_data_job.remove.assert_any_call()
+        background_job_manager._update_system_data_job.remove.assert_any_call()
+        background_job_manager._update_chunks_data_job.remove.assert_any_call()
+        background_job_manager._update_storage_data_job.remove.assert_any_call()
+        background_job_manager._update_plugin_log_job.remove.assert_any_call()
+        mock_scheduler.shutdown.assert_called()
