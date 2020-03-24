8b0263e734087c996caae8c3b80ed1f78416a59f
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-03-24 13:12:46 +0000

    Add first draft
    Co-authored-by: caterinamandel98 <38725641+caterinamandel98@users.noreply.github.com>

--- tests/database_manager/test_background_scheduler.py
+++ tests/database_manager/test_background_scheduler.py
@@ -1,43 +1,65 @@
 """Tests for the background_scheduler module."""
 
 from multiprocessing import Value
+from unittest.mock import MagicMock, patch
 
-from psycopg2 import pool
 from pytest import fixture
 
 from hyrisecockpit.database_manager.background_scheduler import BackgroundJobManager
-from hyrisecockpit.settings import (
-    STORAGE_HOST,
-    STORAGE_PASSWORD,
-    STORAGE_PORT,
-    STORAGE_USER,
-)
 
 
 class TextBackgroundJobManager:
     """Tests BackgroundJobManager class."""
 
+    def get_database_id(self) -> str:
+        """Return database id."""
+        return "MongoDB"
+
+    def get_database_blocked(self) -> Value:
+        """Return blocked database Value."""
+        return Value("b", False)
+
+    def get_connection_pool(self) -> MagicMock:
+        """Return connection pool."""
+        return MagicMock()
+
+    def get_storage_host(self) -> str:
+        """Return storage host."""
+        return "influx"
+
+    def get_storage_password(self) -> str:
+        """Return mocked storage password."""
+        return "password"
+
+    def get_storage_port(self) -> str:
+        """Return mocked storage port."""
+        return "4321"
+
+    def get_storage_user(self) -> str:
+        """Return mocked storage user."""
+        return "user?"
+
     @fixture
-    def connection_pool(self) -> pool:
-        """Get a mock connection pool."""
-        return pool.ThreadedConnectionPool(
-            0, 5, "user", "password", "host", "port", "dbname",
+    @patch(
+        "hyrisecockpit.database_manager.background_scheduler.BackgroundScheduler",
+        MagicMock(),
+    )
+    @patch(
+        "hyrisecockpit.database_manager.background_scheduler.BackgroundJobManager._init_jobs",
+        MagicMock(),
+    )
+    def background_job_manager(self) -> BackgroundJobManager:
+        """Get a new BackgrpundJobManager."""
+        return BackgroundJobManager(
+            self.get_database_id(),
+            self.get_database_blocked(),
+            self.get_connection_pool(),
+            self.get_storage_host(),
+            self.get_storage_password(),
+            self.get_storage_port(),
+            self.get_storage_user(),
         )
 
-    @fixture
-    def background_job_manager(self) -> BackgroundJobManager:
-        """Get a new DatabaseManager."""
-        with BackgroundJobManager(
-            "database",
-            Value("b", False),
-            self.connection_pool,
-            STORAGE_HOST,
-            STORAGE_PASSWORD,
-            STORAGE_PORT,
-            STORAGE_USER,
-        ) as background_job_manager:
-            return background_job_manager
-
     def test_initializes(self, background_job_manager: BackgroundJobManager) -> None:
-        """A DatabaseManager initializes."""
+        """A BackgroundJobManager initializes."""
         assert isinstance(background_job_manager, BackgroundJobManager)
