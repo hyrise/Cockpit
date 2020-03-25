1fa9abd2390e76a6dcc0cbed21846d0df29588fa
fabianhe <fabian.heseding@web.de>
2020-03-25 16:47:09 +0100

    Add workload entity

--- hyrisecockpit/api/app/routes.py
+++ hyrisecockpit/api/app/routes.py
@@ -1,6 +1,11 @@
 """Method to register all routes of this app."""
 
+from flask import Flask
+from flask_restx import Api
 
-def register_routes(api, app, root="api") -> None:
+
+def register_routes(api: Api, app: Flask, root: str = "api") -> None:
     """Register all sub-routes."""
-    pass
+    from .workload import register_routes as attach_workload
+
+    attach_workload(api, app, root)
