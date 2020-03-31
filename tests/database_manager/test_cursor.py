7fb0d82da5f546a95c808a85e6c455f1a51364f9
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-03-31 09:42:45 +0000

    Add tests for pool cursor

--- tests/database_manager/test_cursor.py
+++ tests/database_manager/test_cursor.py
@@ -1,14 +1,24 @@
 """Tests for the cursor module."""
 from typing import Any, Dict, List, Tuple
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
-from pytest import mark
+from pandas import DataFrame
+from psycopg2 import DatabaseError, InterfaceError
+from pytest import fixture, mark
 
-from hyrisecockpit.database_manager.cursor import StorageCursor
+from hyrisecockpit.database_manager.cursor import PoolCursor, StorageCursor
 
 
 class TestCursor:
     """Tests Cursor classes."""
+
+    @fixture
+    @patch(
+        "hyrisecockpit.database_manager.cursor.PoolCursor._initialize", MagicMock(),
+    )
+    def pool_cursor(self) -> PoolCursor:
+        """Return patched pool cursor object."""
+        return PoolCursor(MagicMock())
 
     @mark.parametrize(
         "queries",
@@ -134,3 +144,219 @@ class TestCursor:
         cursor._connection.write_points.assert_called_once_with(
             [expected_point], database="database"
         )
+
+    @patch("hyrisecockpit.database_manager.cursor.PoolCursor._initialize",)
+    def test_initializes_pool_cursor_correctly(self, mocked_initialize) -> None:
+        """Test initializes pool cursor correctly."""
+        mocked_pool = MagicMock()
+        pool_cursor = PoolCursor(mocked_pool)
+
+        pool_cursor.pool == mocked_pool
+        mocked_initialize.assert_called_once()
+
+    def test_initializes_connection_correctly_with_no_exception(self) -> None:
+        """Test initialize connection and cursor correctly."""
+        mocked_pool = MagicMock()
+        mocked_connection = MagicMock()
+        mocked_cursor = MagicMock()
+
+        mocked_connection.cursor.return_value = mocked_cursor
+        mocked_pool.getconn.return_value = mocked_connection
+
+        pool_cursor = PoolCursor(mocked_pool)
+
+        assert pool_cursor._connection == mocked_connection
+        mocked_connection.set_session.assert_called_once_with(autocommit=True)
+        assert pool_cursor._cur == mocked_cursor
+        assert pool_cursor.valid
+
+    @mark.parametrize(
+        "exceptions", [DatabaseError(), InterfaceError()],
+    )
+    def test_initializes_connection_with_exception(self, exceptions) -> None:
+        """Test initialize connection witch exception."""
+
+        def raise_exception(*args):
+            """Throw exception."""
+            raise exceptions
+
+        mocked_pool = MagicMock()
+        mocked_connection = MagicMock()
+        mocked_cursor = MagicMock()
+
+        mocked_connection.cursor.return_value = mocked_cursor
+        mocked_pool.getconn = raise_exception
+
+        pool_cursor = PoolCursor(mocked_pool)
+
+        assert pool_cursor._connection is None
+        assert pool_cursor._cur is None
+        assert not pool_cursor.valid
+
+    def test_executes_with_no_valid_pool_cursor(self, pool_cursor) -> None:
+        """Test execute with no valid pool cursor."""
+        pool_cursor.valid = False
+        pool_cursor._cur = MagicMock()
+
+        results = pool_cursor.execute("query", None)
+
+        assert results is None
+        pool_cursor._cur.execute.assert_not_called()
+
+    def test_executes_with_pool_cursor(self, pool_cursor) -> None:
+        """Test execute witch valid pool cursor and no exception."""
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.execute.return_value = None
+
+        results = pool_cursor.execute("query", None)
+
+        assert results is None
+        pool_cursor._cur.execute.assert_called_once_with("query", None)
+
+    @mark.parametrize(
+        "exceptions", [DatabaseError(), InterfaceError()],
+    )
+    def test_execute_with_exception(self, pool_cursor, exceptions) -> None:
+        """Test execute with exceptions."""
+
+        def raise_exception(*args):
+            """Throw exception."""
+            raise exceptions
+
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.execute = raise_exception
+
+        results = pool_cursor.execute("query", None)
+
+        assert results is None
+
+    def test_fetches_one_with_no_valid_pool_cursor(self, pool_cursor) -> None:
+        """Test fetch one with no valid pool cursor."""
+        pool_cursor.valid = False
+        pool_cursor._cur = MagicMock()
+
+        results = pool_cursor.fetchone()
+
+        assert results == ()
+        pool_cursor._cur.fetchone.assert_not_called()
+
+    def test_fetches_one_with_pool_cursor(self, pool_cursor) -> None:
+        """Test fetch one witch valid pool cursor and no exception."""
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.fetchone.return_value = ("hallo",)
+
+        results = pool_cursor.fetchone()
+
+        assert results == ("hallo",)
+        pool_cursor._cur.fetchone.assert_called_once()
+
+    @mark.parametrize(
+        "exceptions", [DatabaseError(), InterfaceError()],
+    )
+    def test_fetches_one_with_exception(self, pool_cursor, exceptions) -> None:
+        """Test fetch one with exceptions."""
+
+        def raise_exception(*args):
+            """Throw exception."""
+            raise exceptions
+
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.fetchone = raise_exception
+
+        results = pool_cursor.fetchone()
+
+        assert results == ()
+
+    def test_fetches_all_with_no_valid_pool_cursor(self, pool_cursor) -> None:
+        """Test fetch all with no valid pool cursor."""
+        pool_cursor.valid = False
+        pool_cursor._cur = MagicMock()
+
+        results = pool_cursor.fetchall()
+
+        assert results == []
+        pool_cursor._cur.fetchall.assert_not_called()
+
+    def test_fetches_all_with_pool_cursor(self, pool_cursor) -> None:
+        """Test fetch all witch valid pool cursor and no exception."""
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.fetchall.return_value = [("hallo",), ("world",)]
+
+        results = pool_cursor.fetchall()
+
+        assert results == [("hallo",), ("world",)]
+        pool_cursor._cur.fetchall.assert_called_once()
+
+    @mark.parametrize(
+        "exceptions", [DatabaseError(), InterfaceError()],
+    )
+    def test_fetches_all_with_exception(self, pool_cursor, exceptions) -> None:
+        """Test fetch all with exceptions."""
+
+        def raise_exception(*args):
+            """Throw exception."""
+            raise exceptions
+
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._cur.fetchall = raise_exception
+
+        results = pool_cursor.fetchall()
+
+        assert results == []
+
+    def test_reads_sql_query_with_no_valid_pool_cursor(self, pool_cursor) -> None:
+        """Test read sql query with no valid pool cursor."""
+        pool_cursor.valid = False
+        pool_cursor._cur = MagicMock()
+
+        results = pool_cursor.read_sql_query("query")
+
+        assert results.empty
+        assert type(results) is DataFrame
+        pool_cursor._cur.read_sql_query_pandas.assert_not_called()
+
+    @patch("hyrisecockpit.database_manager.cursor.read_sql_query_pandas",)
+    def test_reads_sql_query_with_pool_cursor(
+        self, mocked_read_sql_query_pandas, pool_cursor
+    ) -> None:
+        """Test read sql query witch valid pool cursor and no exception."""
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        fake_df = DataFrame({"hallo": [1, 2]})
+        moked_connection = MagicMock()
+        pool_cursor._connection = moked_connection
+        mocked_read_sql_query_pandas.return_value = fake_df
+
+        results = pool_cursor.read_sql_query("query")
+
+        assert results.equals(fake_df)
+        mocked_read_sql_query_pandas.assert_called_once_with("query", moked_connection)
+
+    @patch("hyrisecockpit.database_manager.cursor.read_sql_query_pandas",)
+    @mark.parametrize(
+        "exceptions", [DatabaseError(), InterfaceError()],
+    )
+    def test_reads_sql_query_with_exception(
+        self, mocked_read_sql_query_pandas, pool_cursor, exceptions
+    ) -> None:
+        """Test read sql query with exceptions."""
+
+        def raise_exception(*args):
+            """Throw exception."""
+            raise exceptions
+
+        pool_cursor.valid = True
+        pool_cursor._cur = MagicMock()
+        pool_cursor._connection = MagicMock()
+        mocked_read_sql_query_pandas.side_effect = raise_exception
+
+        results = pool_cursor.read_sql_query("query")
+
+        assert results.empty
+        assert type(results) is DataFrame
