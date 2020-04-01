a7a14990ffef15988f8f44ff1a5ad6270345e5e0
Mona <mona@sobhani.net>
2020-04-01 12:57:21 +0200

    Revert "change @click"
    This reverts commit aaf66dee483675b59d0ed1fb5759df304c7c69ed.

--- hyrisecockpit/frontend/src/views/AppDrawer.vue
+++ hyrisecockpit/frontend/src/views/AppDrawer.vue
@@ -108,7 +108,7 @@
 
       <v-list-item
         id="workload-generation-button"
-        @click="showWorkloadDialog = true"
+        @click="openWorkloadDialog()"
       >
         <v-list-item-icon>
           <v-icon>mdi-account-cog</v-icon>
