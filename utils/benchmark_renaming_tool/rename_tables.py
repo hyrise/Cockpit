505fde3c6d8aa6b0e4e81e3f2d3408b3bdcc7bde
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-07 15:51:31 +0200

    Add rename tables
    Co-authored-by: caterinamandel98 <38725641+caterinamandel98@users.noreply.github.com>

---
+++ utils/benchmark_renaming_tool/rename_tables.py
@@ -0,0 +1,16 @@
+"""Module to get commandline arguments and start replacer."""
+from utils.benchmark_renaming_tool.argument_parser import ArgumentParser
+from utils.benchmark_renaming_tool.replacer import Replacer
+from utils.endpoint_benchmark.print_colors import print_yellow
+
+if __name__ == "__main__":
+
+    argp = ArgumentParser()
+    config = argp.get_configuration()
+    if not config:
+        exit()
+
+    print_yellow(f"\nReplace {config['workload']} tables to scale {config['scale']} \n")
+    Replacer(config).start()
+
+    print("\nAll done" + " \N{grinning face with smiling eyes}\n")
