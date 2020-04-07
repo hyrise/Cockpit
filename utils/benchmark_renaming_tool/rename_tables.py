4618182c1feb36276bef8ff941784a311ee462f4
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-07 15:58:54 +0200

    Rename parser
    Co-authored-by: caterinamandel98 <38725641+caterinamandel98@users.noreply.github.com>

--- utils/benchmark_renaming_tool/rename_tables.py
+++ utils/benchmark_renaming_tool/rename_tables.py
@@ -1,5 +1,5 @@
 """Module to get commandline arguments and start replacer."""
-from utils.benchmark_renaming_tool.argument_parser import ArgumentParser
+from utils.benchmark_renaming_tool.rename_argument_parser import ArgumentParser
 from utils.benchmark_renaming_tool.replacer import Replacer
 from utils.endpoint_benchmark.print_colors import print_yellow
 
