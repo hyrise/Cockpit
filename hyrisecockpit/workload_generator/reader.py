55d7ddc7b25c09c75ffa959b1b92a1c9df21dd23
fabianhe <fabian.heseding@web.de>
2020-04-07 14:38:53 +0200

    Add new reader

--- hyrisecockpit/workload_generator/reader.py
+++ hyrisecockpit/workload_generator/reader.py
@@ -1,75 +1,52 @@
-"""Module for reading queries."""
-from os import fsdecode, fsencode, listdir
-from os.path import dirname, exists, splitext
-from typing import Any, Dict, List
-
-from hyrisecockpit.exception import (
-    EmptyWorkloadFolderException,
-    NotExistingWorkloadFolderException,
-)
+"""Module for reading workload queries."""
+from os import listdir
+from os.path import dirname, isdir, isfile, join, splitext
+from typing import Dict, List, Optional
 
 
-class WorkloadReader(object):
-    """Generic workload reader."""
+class WorkloadReader:
+    """Reads queries from workloads."""
 
-    def _create_absolute_workload_path(self, relative_workload_path: str) -> str:
-        """Create absolute path to the folder."""
-        absolute_workload_reader_path: str = dirname(__file__)
-        return f"{absolute_workload_reader_path}/{relative_workload_path}"
+    location: str = join(dirname(__file__), "workloads", "workload_queries")
 
-    def _get_workload_folder(
-        self, absolute_workload_path: str, workload_type: str
-    ) -> Any:
-        """Check if folder exists or is emty and returns it."""
-        if not exists(absolute_workload_path):
-            raise NotExistingWorkloadFolderException(
-                f"Workload {workload_type} not found: directory doesn't exist"
-            )
-        directory = fsencode(absolute_workload_path)
-        if len(listdir(directory)) == 0:
-            raise EmptyWorkloadFolderException(
-                f"Workload {workload_type} directory is empty"
-            )
-        return listdir(directory)
+    @classmethod
+    def __read_query(cls, path_to_file: str) -> List[str]:
+        with open(path_to_file, "r") as f:
+            raw_queries: str = f.read()
+        return [
+            transaction for transaction in raw_queries.split("---") if transaction != ""
+        ]
 
-    def _read_content_of_workload_folder(
-        self,
-        workload_folder: Any,
-        absolute_workload_path: str,
-        delimiter: str,
-        file_type: str,
-    ) -> Dict[str, List[str]]:
-        """Read every file in folder and returns content."""
-        queries: Dict[str, List[str]] = {}
-        for file in workload_folder:
-            filename: str = fsdecode(file)
-            if filename.endswith(f".{file_type}"):
-                with open(f"{absolute_workload_path}/{filename}", "r") as f:
-                    raw_queries: str = f.read()
-                transactions = [
-                    transaction
-                    for transaction in raw_queries.split(delimiter)
-                    if transaction != ""
-                ]
-                queries[splitext(filename)[0]] = transactions
-        return queries
+    @classmethod
+    def available(cls) -> Dict[str, List[str]]:
+        """Get all available workloads with their queries.
 
-    def read_workload(
-        self,
-        relative_workload_path: str,
-        delimiter: str,
-        file_type: str,
-        workload_type: str,
-    ) -> Dict[str, List[str]]:
-        """Read sql queries from files in folder."""
-        absolute_workload_path: str = self._create_absolute_workload_path(
-            relative_workload_path
-        )
-        workload_folder = self._get_workload_folder(
-            absolute_workload_path, workload_type
-        )
-        workload: Dict[str, List[str]] = self._read_content_of_workload_folder(
-            workload_folder, absolute_workload_path, delimiter, file_type
-        )
+        Returns a dict mapping workload folder names to a list of all query file names.
+        """
+        return {
+            workload: [
+                queries
+                for queries in listdir(join(cls.location, workload))
+                if isfile(join(cls.location, workload, queries))
+                and splitext(queries)[-1] == ".sql"
+            ]
+            for workload in listdir(cls.location)
+            if isdir(join(cls.location, workload))
+        }
 
-        return workload
+    @classmethod
+    def get(cls, workload: str) -> Optional[Dict[str, List[str]]]:
+        """Get a workload from the workload folder.
+
+        Returns the query names and a list of all queries.
+        Returns `None` if the workload could not be found.
+        """
+        try:
+            return {
+                splitext(queries)[0]: cls.__read_query(
+                    join(cls.location, workload, queries)
+                )
+                for queries in cls.available()[workload]
+            }
+        except KeyError:
+            return None
