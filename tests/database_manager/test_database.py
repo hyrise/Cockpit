56ed6de1f1b97f4453688e1517d42ccdda3dd1bf
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-08 08:05:28 +0000

    Can you please work hook? Thank you...

--- tests/database_manager/test_database.py
+++ tests/database_manager/test_database.py
@@ -52,6 +52,7 @@ def get_mocked_pool_cursor(*args) -> MagicMock:
 def reset_mocked_pool_cursor() -> None:
     """Reset mocked pool cursor."""
     global mocked_pool_cur
+
     mocked_pool_cur = MagicMock()
     mocked_pool_cur.fetchall.return_value = []
 
