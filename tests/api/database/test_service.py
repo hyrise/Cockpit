a76d4ece56354f7d4847a3b72c46e008d95c5804
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-11 14:41:48 +0000

    Adjust tests

--- tests/api/database/test_service.py
+++ tests/api/database/test_service.py
@@ -5,23 +5,14 @@ from pytest import fixture
 
 from hyrisecockpit.api.app.database.service import DatabaseService
 
-mocked_socket_context: MagicMock = MagicMock()
-mocked_context: MagicMock = MagicMock()
-mocked_socket: MagicMock = MagicMock()
-mocked_context.socket.return_value = mocked_socket
-mocked_socket_context.return_value = mocked_context
+mocked_socket = MagicMock()
 
 
-def reset_socket_context() -> None:
-    """Reset mocked socket context."""
-    global mocked_socket_context
-    global mocked_context
-    global mocked_socket
-    mocked_socket_context = MagicMock()
-    mocked_context = MagicMock()
-    mocked_socket = MagicMock()
-    mocked_context.socket.return_value = mocked_socket
-    mocked_socket_context.return_value = mocked_context
+def get_mocked_socket_manager(*args):
+    """Return fake storage cursor."""
+    mocked_socket_manager_constructor: MagicMock = MagicMock()
+    mocked_socket_manager_constructor.__enter__.return_value: MagicMock = mocked_socket
+    return mocked_socket_manager_constructor
 
 
 @fixture
@@ -33,60 +24,24 @@ def database_service():
 class TestDatabaseService:
     """Tests for the database service."""
 
-    @patch("hyrisecockpit.api.app.database.service.Context", mocked_socket_context)
-    @patch("hyrisecockpit.api.app.database.service.REQ", "fake_req")
+    @patch("hyrisecockpit.api.app.database.service.validate")
     @patch(
-        "hyrisecockpit.api.app.database.service.url", "some_url",
+        "hyrisecockpit.api.app.database.service.ManagerSocket",
+        get_mocked_socket_manager,
     )
-    def test_opens_socket_connection(self, database_service: DatabaseService) -> None:
-        """Test that socket connection is created correctly."""
-        global mocked_socket_context
-        global mocked_context
-        global mocked_socket
-
-        response = database_service._open_socket_connection()
-
-        mocked_socket_context.assert_called_once_with(io_threads=1)
-        mocked_context.socket.assert_called_once_with("fake_req")
-        mocked_socket.connect.assert_called_once_with("some_url")
-
-        assert response == mocked_socket
-
-        reset_socket_context()
-
-    @patch(
-        "hyrisecockpit.api.app.database.service.url", "some_url",
-    )
-    def test_closes_socket_connection(self, database_service: DatabaseService) -> None:
-        """Test that socket connection is closed correctly."""
-        mocked_socket: MagicMock = MagicMock()
-
-        database_service._close_socket_connection(mocked_socket)
-
-        mocked_socket.disconnect.assert_called_once_with("some_url")
-        mocked_socket.close.assert_called_once_with()
-
-    @patch(
-        "hyrisecockpit.api.app.database.service.url", "some_url",
-    )
-    @patch("hyrisecockpit.api.app.database.service.DatabaseService",)
     def test_sends_request(
-        self, mocked_database_service: MagicMock, database_service: DatabaseService
+        self, mocked_validate: MagicMock, database_service: DatabaseService
     ) -> None:
-        """Test sending of request."""
+        """Test sending of message."""
         fake_message = "do something"
+        global mocked_socket
+        mocked_socket.send_message.return_value = {"some": "response"}
 
-        mocked_socket: MagicMock = MagicMock()
-        mocked_socket.recv_json.return_value = {"some": "json"}
-        mocked_database_service._open_socket_connection.return_value = mocked_socket
+        response = database_service._send_message(fake_message)  # type: ignore
 
-        response = database_service._send_req(fake_message)  # type: ignore
+        mocked_socket.send_message.assert_called_once_with(fake_message)
+        mocked_validate.assert_called_once()
 
-        mocked_socket.send_json.assert_called_once_with(fake_message)
-        mocked_socket.recv_json.assert_called_once()
-        mocked_database_service._open_socket_connection.assert_called_once()
-        mocked_database_service._close_socket_connection.assert_called_once_with(
-            mocked_socket
-        )
+        assert response == {"some": "response"}  # type: ignore
 
-        assert response == {"some": "json"}  # type: ignore
+        mocked_socket = MagicMock()
