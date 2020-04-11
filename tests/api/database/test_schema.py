818721df601c062bd8dbae76453bd4747c49c68d
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-11 10:09:52 +0000

    Add last tests for schema

--- tests/api/database/test_schema.py
+++ tests/api/database/test_schema.py
@@ -4,6 +4,7 @@ from pytest import fixture, mark
 
 from hyrisecockpit.api.app.database.interface import (
     AvailableBenchmarkTablesInterface,
+    BenchmarkTablesInterface,
     DatabaseInterface,
     DetailedDatabaseInterface,
 )
@@ -129,3 +130,73 @@ class TestDatabaseSchema:
         )
         assert isinstance(available_benchmark_tables, AvailableBenchmarkTables)
         assert vars(available_benchmark_tables) == available_benchmark_tables_interface
+
+    @mark.parametrize(
+        "benchmark_tables_interface", [{"folder_name": "Tina"}, {"folder_name": "Bibi"}]
+    )
+    def test_deserializes_benchmark_tables_schema(
+        self,
+        benchmark_tables_schema: BenchmarkTablesSchema,
+        benchmark_tables_interface: BenchmarkTablesInterface,
+    ) -> None:
+        """A benchmark tables schema can create a database model."""
+        benchmark_tables = benchmark_tables_schema.load(benchmark_tables_interface)
+        assert (
+            benchmark_tables["folder_name"] == benchmark_tables_interface["folder_name"]
+        )
+
+    @mark.parametrize("database_interface", [{"id": "Tina"}, {"id": "Bibi"}])
+    def test_serializes_database(
+        self, database_schema: DatabaseSchmea, database_interface: DatabaseInterface
+    ) -> None:
+        """A database model can be serialized with a database schema."""
+        database: Database = Database(**database_interface)
+        serialized = database_schema.dump(database)
+        assert vars(database) == serialized == database_interface
+
+    @mark.parametrize(
+        "detailed_database_interface", [database_one_parms, database_two_parms]
+    )
+    def test_serializes_detailed_database(
+        self,
+        detailed_database_schema: DetailedDatabaseSchema,
+        detailed_database_interface: DetailedDatabaseInterface,
+    ) -> None:
+        """A detailed database model can be serialized with a detailed database schema."""
+        detailed_database: DetailedDatabase = DetailedDatabase(
+            **detailed_database_interface
+        )
+        serialized = detailed_database_schema.dump(detailed_database)
+        assert vars(detailed_database) == serialized == detailed_database_interface
+
+    @mark.parametrize(
+        "available_benchmark_tables_interface",
+        [{"folder_names": ["hallo", "world"]}, {"folder_names": ["ping", "pong"]}],
+    )
+    def test_serializes_available_benchmark_tables(
+        self,
+        available_benchmark_tables_schema: AvailableBenchmarkTablesSchema,
+        available_benchmark_tables_interface: AvailableBenchmarkTablesInterface,
+    ) -> None:
+        """A available benchmark tables model can be serialized with a available benchmark tables schema."""
+        available_benchmark_tables: AvailableBenchmarkTables = AvailableBenchmarkTables(
+            **available_benchmark_tables_interface
+        )
+        serialized = available_benchmark_tables_schema.dump(available_benchmark_tables)
+        assert (
+            vars(available_benchmark_tables)
+            == serialized
+            == available_benchmark_tables_interface
+        )
+
+    @mark.parametrize(
+        "benchmark_tables_interface", [{"folder_name": "Tina"}, {"folder_name": "Bibi"}]
+    )
+    def test_serializes_benchmark_tables(
+        self,
+        benchmark_tables_schema: BenchmarkTablesSchema,
+        benchmark_tables_interface: BenchmarkTablesInterface,
+    ) -> None:
+        """A benchmark_tables dictionary can be serialized with a benchmark tables schema."""
+        serialized = benchmark_tables_schema.dump(benchmark_tables_interface)
+        assert serialized == benchmark_tables_interface
