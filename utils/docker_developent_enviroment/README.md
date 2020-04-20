216a93f74f55c0f13225f7f92f4cb1a9481fa1d8
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-04-20 16:38:21 +0200

    Rmove normal and complete dockerfile and adjust boot script

--- utils/docker_developent_enviroment/README.md
+++ utils/docker_developent_enviroment/README.md
@@ -18,15 +18,8 @@ https://docs.docker.com/docker-for-mac/install/
 
 The idea is to mount our working directory and work inside the ubuntu system of the container. So we will git pull and commit from inside the container.
 
-There are three docker images to
+## Image
 
-* `cockpit_minimal` it just gives us a simple environment with all packages for python and javascript
-* `cockpit_normal` in addition to minimal an influxdb is installed .
-* `cockpit_complete` in addition to normal two hyrise databases are ready to go.
-
-## Images
-
-It is important to mention that the hyrise server in the image  is compiled for the specific architecture. So it is possible that it wound run on your system. In that case you need to build your own image with the dockerfile. 
 
 ### Build images
 
@@ -34,8 +27,6 @@ It is important to mention that the hyrise server in the image  is compiled for 
 (To get the image just type the following command in your terminal:  
 
 * `docker pull dubrawski/cockpit_minimal_intel_xeon:1.0`
-* `docker pull dubrawski/cockpit_normal_intel_xeon:1.0`
-* `docker pull dubrawski/cockpit_complete_intel_xeon:1.0`
 
 #### Mac osx
 Coming in the future.
@@ -43,10 +34,8 @@ For the moment you need to build it on the mac
  
 ### Build you own image 
 
-1. navigate in the folder for the image you want to build (for example `cd cockpit/utils/docker_developer_enviroment/normal`)
-2. run `docker image build --tag cockpit_normal .`
-
-The build process for the `minimal` and `normal` image can take up to 5 to 10 minutes. The build time for the `complete` image up to one hour (hyrise compile time).
+1. navigate in the folder for the image you want to build (for example `cd cockpit/utils/docker_developer_enviroment`)
+2. run `docker image build --tag docker_cockpit .`
 
 After you build an image you can always use it to start an container. 
 
@@ -56,7 +45,7 @@ After you build an image you can always use it to start an container.
 2. Set up .env 
 3. Start the container with: 
 	```
-	docker container run --mount type=bind,source=/path/to/cloned/Cockpit,destination=/cockpit --name cockpit_container  --interactive --tty -p 8000:8000 dubrawski/cockpit_complete bash
+	docker container run --mount type=bind,source=/path/to/cloned/Cockpit,destination=/cockpit --name cockpit_container  --interactive --tty -p 8000:8000 dubrawski/cockpit_minimal_intel_xeon:1.0 bash
 	```
 	*I would recommend using setting up an alias for this command.*
 	 
@@ -87,12 +76,9 @@ Inside the container you can use the boot tool in:
 The basic usage is: 
 
 ```
-optional arguments:
-  -h, --help  show this help message and exit
-  -s          start Hyrise server (default: False)
-  -i          start influx (default: False)
-  -b          start backend (default: False)
-  -all        start backend (default: False)
+    python boot_tool.py
 ```
+
+It will start the backend components. 
 
 
--- utils/docker_developent_enviroment/boot_cockpit.py
+++ utils/docker_developent_enviroment/boot_cockpit.py
@@ -1,6 +1,5 @@
 """This module start the components inside the docker container."""
 
-import argparse
 import signal
 import sys
 from subprocess import DEVNULL, Popen  # nosec
@@ -9,64 +8,14 @@ from time import sleep
 subprocesses = []
 
 
-class ArgumentParser:
-    """Parse arguments from command line."""
-
-    def __init__(self):
-        """Initialize a ArgumentParser."""
-        self.parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-        )
-        self._add_arguments()
-
-    def _add_arguments(self):
-        self.parser.add_argument(
-            "-s", action="store_true", help="start Hyrise server",
-        )
-        self.parser.add_argument(
-            "-i", action="store_true", help="start influx",
-        )
-        self.parser.add_argument(
-            "-b", action="store_true", help="start backend",
-        )
-        self.parser.add_argument(
-            "-all", action="store_true", help="start backend",
-        )
-
-    def get_coniguration(self):
-        """Return which component to start."""
-        args, leftovers = self.parser.parse_known_args()
-        arguments = vars(args)
-        if arguments["all"]:
-            return {"s": True, "i": True, "b": True}
-        return arguments
+def print_green(value):
+    """Print green colored text."""
+    print("\033[92m{}\033[00m".format(value))
 
 
-def start_hyriseserver():
-    """Start hyrise server."""
-    hyrise_one_prosess = Popen(  # nosec
-        ["./hyriseServer"],
-        cwd="/hyrise_server/hyrise/cmake-build-release",
-        stderr=DEVNULL,
-        stdout=DEVNULL,
-    )
-    subprocesses.append(hyrise_one_prosess)
-    sleep(0.5)
-    hyrise_two_prosess = Popen(  # nosec
-        ["./hyriseServer"],
-        cwd="/hyrise_server/hyrise_two/cmake-build-release",
-        stderr=DEVNULL,
-        stdout=DEVNULL,
-    )
-    subprocesses.append(hyrise_two_prosess)
-    sleep(0.5)
-
-
-def start_influxdb():
-    """Start InfluxDB."""
-    influxdb_process = Popen(["influxd"], stderr=DEVNULL, stdout=DEVNULL,)  # nosec
-    subprocesses.append(influxdb_process)
-    sleep(0.5)
+def print_cyan(value):
+    """Print cyan colored text."""
+    print("\033[96m{}\033[00m".format(value))
 
 
 def start_backend():
@@ -76,16 +25,19 @@ def start_backend():
     )
     subprocesses.append(backend_prosess)
     sleep(1)
+    print_green("cockpit-backend running " + "\N{check mark}")
     manager_process = Popen(  # nosec
         ["pipenv", "run", "cockpit-manager"], stderr=DEVNULL, stdout=DEVNULL,
     )
     subprocesses.append(manager_process)
     sleep(0.5)
+    print_green("cockpit-manager running " + "\N{check mark}")
     generator_process = Popen(  # nosec
         ["pipenv", "run", "cockpit-generator"], stderr=DEVNULL, stdout=DEVNULL,
     )
     subprocesses.append(generator_process)
     sleep(0.5)
+    print_green("cockpit-generator running " + "\N{check mark}")
 
 
 def close_components() -> None:
@@ -102,30 +54,9 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-def print_green(value):
-    """Print green colored text."""
-    print("\033[92m{}\033[00m".format(value))
-
-
-def print_cyan(value):
-    """Print cyan colored text."""
-    print("\033[96m{}\033[00m".format(value))
-
-
 if __name__ == "__main__":
-    coniguration = ArgumentParser().get_coniguration()  # type: ignore
-    if coniguration["s"]:
-        print_cyan("Starting Hyrise server...")  # type: ignore
-        start_hyriseserver()  # type: ignore
-        print_green("Hyrise Server running " + "\N{check mark}")  # type: ignore
-    if coniguration["i"]:
-        print_cyan("Starting INfluxdb...")  # type: ignore
-        start_influxdb()  # type: ignore
-        print_green("INfluxdb running " + "\N{check mark}")  # type: ignore
-    if coniguration["b"]:
-        print_cyan("Starting Backend-Components...")  # type: ignore
-        start_backend()  # type: ignore
-        print_green("Backend-Components running " + "\N{check mark}")  # type: ignore
+    print_cyan("Starting backend components...")  # type: ignore
+    start_backend()  # type: ignore
     signal.signal(signal.SIGINT, signal_handler)
     print("All Components running.")
     print("Press Ctrl+C to close components.")
