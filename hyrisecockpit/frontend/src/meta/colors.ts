9d45ff4e48c7b8bc9d886cc39db7242bb07e9549
monasobh <Mona.Sobhani@student.hpi.uni-potsdam.de>
2020-04-20 14:34:18 +0200

    differenciate green, orange, red from ColorDefinition

--- hyrisecockpit/frontend/src/meta/colors.ts
+++ hyrisecockpit/frontend/src/meta/colors.ts
@@ -7,27 +7,33 @@ export const colorValueDefinition: Record<string, string> = {
   low: "#ff6666", // red
   success: "#4ca64c", //green
   warning: "#ffc04c", //orange
-  error: "ff4c4c" //red
-};
-
-export const colorDefinition: Record<string, string> = {
-  green: "#b2df8a",
-  red: "#fb9a99",
+  error: "ff4c4c", //red
   orange: "#fdbf6f", //heatmap
   darkorange: "#e9b062", //heatmap
   blue: "#a6cee3", //heatmap,
   darkblue: "#95b9cc", //heatmap, treemap
+};
+
+export const colorDefinition: Record<string, string> = {
+  oceanblue: "#99d8c9",//"#a3dbce"//"#addfd3",//"#b7e3d9",
+  mintgreen2: "#abe3bc",//"#b7e8c9",//"#c0ebd0",
+  greengrey: "#b8ccc4",//"#cbd6cf",//"#cbbdb5",
+  fireplace: "#fb87be",//"#fc9680",
+
+
+  //green: "#b2df8a",
+  //red: "#fb9a99",
+
+
   violet: "#eec9cf",
   purple: "#e5bbd0",
   pink: "#fbbbcb",
   skincolor: "#fcc6b2",
-  fireplace: "#fc9680",
+  
   mintgreen: "#c2e7cb",
   bluegrey: "#bde4b9",
   turqoise: "#c0ebe5",
-  mintgreen2: "#c0ebd0",
   lightpink: "#ebc0c7",
-  greengrey: "#cbbdb5",
   orange2: "#fec981",
-  oceanblue: "#b7e3d9"
+  
 };
