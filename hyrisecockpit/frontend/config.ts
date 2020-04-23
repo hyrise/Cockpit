b20abc809f22da587b2e7806a5ef36e24e30eebe
kathwill <49514526+kathwill@users.noreply.github.com>
2020-04-23 14:18:54 +0200

    F/fix-workload-endpoints (#551)
    * include new endpoint functionality
    
    * refactoring
    
    * fix update

--- hyrisecockpit/frontend/config.ts
+++ hyrisecockpit/frontend/config.ts
@@ -1,10 +1,11 @@
 const vm = "aurora";
 const port = "8000";
-export const backendUrl = `http://vm-${vm}.eaalab.hpi.uni-potsdam.de:${port}/`;
-//export const backendUrl = "http://127.0.0.1:3000/";
+const backendUrl = `http://vm-${vm}.eaalab.hpi.uni-potsdam.de:${port}/`;
+//const backendUrl = "http://127.0.0.1:3000/";
 
 export const monitorBackend = backendUrl + "monitor/";
 export const controlBackend = backendUrl + "control/";
+export const workloadBackend = backendUrl + "workload/";
 
 export const isInTestMode: boolean =
   (window as any).Cypress || backendUrl.includes("127.0.0.1");
