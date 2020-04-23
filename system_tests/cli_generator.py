205807c0e0145d4d97cf1bcfd7eaa91a7a98d346
PeterTsayun <40639972+PeterTsayun@users.noreply.github.com>
2020-04-23 16:42:29 +0200

    B/Add system tests (#305)
    * Add settings
    
    * Add cli files
    
    * Add wrappers
    
    * Add system tests
    
    * Add stderr and stdout interface
    
    * Disable request logging in Flask
    
    * Use python in the unbuffered mode
    
    * Check stderror after executing tests
    
    * Remove ConnectionError handling
    
    * Use timeout for requests
    
    * Specify imports
    
    * Rename file
    
    * Provide more detailed stderr check
    
    * Add Influx check
    
    * Add table processing status check
    
    * Add execute workload testcase
    
    * Add metrics check while executing a workload
    
    * Add README.md
    
    * Umpdate README.md
    
    * Fix hooks
    
    * Adjust hooks
    
    * Adjust cli_manager.py
    
    * Adjust backend cli
    
    * Adjust stderr checking
    
    * Adjust test add database
    
    * Refactor checking of default tables
    
    * Adjust test execute workload
    
    * Adjust default database
    
    * Use database port
    
    * Add env_example
    
    * Update README
    
    * Adjust tests for historical data
    
    * Add historical metrics check
    
    * Remove deleting of the test_database2
    
    * Adjust timeout
    
    * Adjust cli_generator
    
    * Adjust test initialization
    
    * Adjust test database handling
    
    * Adjust test executes workload
    
    Co-authored-by: Fabian Heseding <39628987+fabianhe@users.noreply.github.com>
    Co-authored-by: caterinamandel98 <38725641+caterinamandel98@users.noreply.github.com>

---
+++ system_tests/cli_generator.py
@@ -0,0 +1,22 @@
+"""CLI used to start the workload generator."""
+from hyrisecockpit.workload_generator.generator import WorkloadGenerator
+from system_tests.settings import (
+    GENERATOR_HOST,
+    GENERATOR_PORT,
+    WORKLOAD_PUBSUB_PORT,
+    WORKLOAD_SUB_HOST,
+)
+
+
+def main() -> None:
+    """Create and start a workload generator."""
+    try:
+        with WorkloadGenerator(
+            GENERATOR_HOST, GENERATOR_PORT, WORKLOAD_SUB_HOST, WORKLOAD_PUBSUB_PORT,
+        ) as workload_generator:
+            workload_generator.start()
+    except KeyboardInterrupt:
+        print("Workload Generator closed.")
+
+
+main()
