78f7e24d71c8967f6ee90b72737903cf71fb38d8
Til Schniese <til.schniese@student.hpi.de>
2020-04-27 21:07:06 +0200

    lint

--- hyrisecockpit/frontend/config.ts
+++ hyrisecockpit/frontend/config.ts
@@ -1,7 +1,7 @@
-const vm = 'aurora';
-const port = '8000';
-const backendUrl = (window as any).Cypress?.env('stubless')
-  ? 'http://127.0.0.1:3000/'
+const vm = "aurora";
+const port = "8000";
+const backendUrl = (window as any).Cypress?.env("stubless")
+  ? "http://127.0.0.1:3000/"
   : `http://vm-${vm}.eaalab.hpi.uni-potsdam.de:${port}/`;
 //const backendUrl = "http://127.0.0.1:3000/";
 
@@ -10,4 +10,4 @@ export const controlBackend = backendUrl + "control/";
 export const workloadBackend = backendUrl + "workload/";
 
 export const isInTestMode: boolean =
-  (window as any).Cypress || backendUrl.includes('127.0.0.1');
+  (window as any).Cypress || backendUrl.includes("127.0.0.1");
