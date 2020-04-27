7442dfb5574ccb8f184a9d6009314358f6d181a4
PeterTsayun <petr.tsayun@student.hpi.de>
2020-04-27 15:10:54 +0000

    Add test fills missing points

--- tests/api/monitor/test_monitor.py
+++ tests/api/monitor/test_monitor.py
@@ -1,6 +1,8 @@
 """Tests for monitor namespace."""
 
-from hyrisecockpit.api.app.monitor.app import get_interval_limits
+from typing import Dict, List
+
+from hyrisecockpit.api.app.monitor.app import fill_missing_points, get_interval_limits
 
 
 class TestMonitor:
@@ -18,3 +20,40 @@ class TestMonitor:
 
         assert startts == 1587997267000000000
         assert endts == 1587997268000000000
+
+    def test_fills_missing_points(self) -> None:
+        """Test filling of the missing points."""
+        startts: int = 1587997260000000000
+        endts: int = 1587997265000000000
+        precision_ns: int = 1000000000
+
+        table_name: str = "table_name"
+        metrics: List[str] = ["metric1", "metric2"]
+        points: List[Dict] = [
+            {"time": 1587997260000000000, "metric1": 1.0, "metric2": 2.0},
+            {"time": 1587997261000000000, "metric1": 3.0, "metric2": 4.0},
+        ]
+        present_timestamps: List[int] = [point["time"] for point in points]
+        expected_points = [
+            {
+                "timestamp": point["time"],
+                "metric1": point["metric1"],
+                "metric2": point["metric2"],
+            }
+            for point in points
+        ]
+
+        filled_points: List[Dict] = fill_missing_points(
+            startts, endts, precision_ns, table_name, metrics, points
+        )
+
+        for timestamp in range(startts, endts, precision_ns):
+            if timestamp not in present_timestamps:
+                assert {
+                    "timestamp": timestamp,
+                    "metric1": 0.0,
+                    "metric2": 0.0,
+                } in filled_points
+
+        for point in expected_points:
+            assert point in filled_points
