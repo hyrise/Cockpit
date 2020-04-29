119a6930ff476d7d7016979307892a80ebb1ec11
PeterTsayun <petr.tsayun@student.hpi.de>
2020-04-29 12:32:41 +0000

    Add cockpit class.

--- plugin_evaluation/cockpit_management/cockpit.py
+++ plugin_evaluation/cockpit_management/cockpit.py
@@ -1,16 +1,27 @@
-"""Module for system tests."""
+"""Module for cockpit management."""
 
 from plugin_evaluation.cockpit_management.cockpit_backend import CockpitBackend
 from plugin_evaluation.cockpit_management.cockpit_generator import CockpitGenerator
 from plugin_evaluation.cockpit_management.cockpit_manager import CockpitManager
 
-backend = CockpitBackend()
-backend.start()
-manager = CockpitManager()
-manager.start()
-generator = CockpitGenerator()
-generator.start()
 
-backend.shutdown()
-manager.shutdown()
-generator.shutdown()
+class Cockpit:
+    """Class for managing of the cockpit components."""
+
+    def __init__(self):
+        """Initialize all cokcpit components."""
+        self.backend = CockpitBackend()
+        self.manager = CockpitManager()
+        self.generator = CockpitGenerator()
+
+    def start(self):
+        """Start all cockpit components."""
+        self.backend.start()
+        self.manager.start()
+        self.generator.start()
+
+    def shutdown(self):
+        """Shutdown all cockpit components."""
+        self.backend.shutdown()
+        self.manager.shutdown()
+        self.generator.shutdown()
