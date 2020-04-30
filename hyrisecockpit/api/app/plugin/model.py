b78eada417d37ddfcc15573959c2f02368c62329
fabianhe <fabian.heseding@web.de>
2020-04-30 15:18:20 +0200

    Add Plugin Settings model

--- hyrisecockpit/api/app/plugin/model.py
+++ hyrisecockpit/api/app/plugin/model.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 class Plugin:
     """Model of a Plugin."""
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         """Initialize a Plugin model."""
         self.name: str = name
 
@@ -13,7 +13,34 @@ class Plugin:
 class PluginID:
     """Model of Plugins per database."""
 
-    def __init__(self, id: str, plugins: Optional[List[Plugin]]):
+    def __init__(self, id: str, plugins: Optional[List[Plugin]]) -> None:
         """Initialize a Plugins per database model."""
         self.id: str = id
         self.plugins: Optional[List[Plugin]] = plugins
+
+
+class PluginSettingBase:
+    """Base model of a Plugin Setting."""
+
+    def __init__(self, name: str, value: str) -> None:
+        """Initialize a Plugin Setting model."""
+        self.name: str = name
+        self.value: str = value
+
+
+class PluginSetting(PluginSettingBase):
+    """Model of a Plugin Setting."""
+
+    def __init__(self, name: str, value: str, description: str) -> None:
+        """Initialize a Plugin Setting model."""
+        super().__init__(name, value)
+        self.description: str = description
+
+
+class DetailedPlugin(Plugin):
+    """Model of a detailed Plugin."""
+
+    def __init__(self, name: str, settings: List[PluginSetting]) -> None:
+        """Initialize a detailed Plugin."""
+        super().__init__(name)
+        self.settings: List[PluginSetting] = settings
--- tests/api/plugin/test_model.py
+++ tests/api/plugin/test_model.py
@@ -3,31 +3,87 @@ from typing import List
 
 from pytest import fixture
 
-from hyrisecockpit.api.app.plugin.model import Plugin, PluginID
+from hyrisecockpit.api.app.plugin.model import (
+    DetailedPlugin,
+    Plugin,
+    PluginID,
+    PluginSetting,
+    PluginSettingBase,
+)
 
 
-@fixture(params=["Clustering", "Compression"],)
-def plugin(request) -> Plugin:
-    """Return a real Plugin model."""
-    return Plugin(name=request.param)
+@fixture(params=["Clustering", "Compression"])
+def name(request) -> str:
+    """Return a Plugin name."""
+    return request.param
 
 
-@fixture(params=[["Clustering"], ["Clustering", "Compression"]])
-def plugins(request) -> List[Plugin]:
-    """Return a List of plugins."""
-    return [Plugin(name=name) for name in request.param]
+@fixture(params=["york", "citadelle"])
+def id(request) -> str:
+    """Return a database id."""
+    return request.param
+
+
+@fixture(params=["MemoryBudget", "MyImaginarySetting"])
+def setting_name(request) -> str:
+    """Return a setting name."""
+    return request.param
+
+
+@fixture(params=["5000", "true"])
+def setting_value(request) -> str:
+    """Return a setting value."""
+    return request.param
+
+
+@fixture(params=["A description of the setting.", ""])
+def setting_description(request) -> str:
+    """Return a setting description."""
+    return request.param
 
 
 @fixture
-def id() -> str:
-    """Return a database id."""
-    return "york"
+def plugin(name: str) -> Plugin:
+    """Return a Plugin model."""
+    return Plugin(name)
+
+
+@fixture
+def plugins(plugin: Plugin) -> List[Plugin]:
+    """Return a list of Plugins."""
+    return [plugin]
 
 
 @fixture
 def plugin_id(id: str, plugins: List[Plugin]) -> PluginID:
-    """Return a pluginID model."""
+    """Return a PluginID model."""
     return PluginID(id=id, plugins=plugins)
+
+
+@fixture
+def plugin_setting_base(setting_name: str, setting_value: str) -> PluginSettingBase:
+    """Return a PluginSettingBase model."""
+    return PluginSettingBase(setting_name, setting_value)
+
+
+@fixture
+def plugin_setting(
+    setting_name: str, setting_value: str, setting_description: str
+) -> PluginSetting:
+    """Return a PluginSetting model."""
+    return PluginSetting(setting_name, setting_value, setting_description)
+
+
+@fixture
+def settings(plugin_setting: PluginSetting) -> List[PluginSetting]:
+    """Return a list of Plugin Settings."""
+    return [plugin_setting]
+
+
+@fixture
+def detailed_plugin(name: str, settings: List[PluginSetting]) -> DetailedPlugin:
+    """Return a DetailedPlugin model."""
+    return DetailedPlugin(name, settings)
 
 
 class TestPlugin:
@@ -44,3 +100,27 @@ class TestPluginID:
     def test_creates(self, plugin_id: PluginID):
         """A PluginID model can be created."""
         assert plugin_id
+
+
+class TestPluginSettingBase:
+    """Tests for the PluginSettingBase model."""
+
+    def test_creates(self, plugin_setting_base: PluginSettingBase):
+        """A PluginSettingBase model can be created."""
+        assert plugin_setting_base
+
+
+class TestPluginSetting:
+    """Tests for the PluginSetting model."""
+
+    def test_creates(self, plugin_setting: PluginSetting):
+        """A PluginSetting model can be created."""
+        assert plugin_setting
+
+
+class TestDetailedPlugin:
+    """Tests for the DetailedPlugin model."""
+
+    def test_creates(self, detailed_plugin: DetailedPlugin):
+        """A DetailedPlugin model can be created."""
+        assert detailed_plugin
