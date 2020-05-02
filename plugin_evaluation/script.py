9a8b29edcb38e9461b11e4c5b8cac8d32b00e5d8
PeterTsayun <petr.tsayun@student.hpi.de>
2020-05-02 19:50:43 +0000

    Improve logging

--- plugin_evaluation/script.py
+++ plugin_evaluation/script.py
@@ -7,46 +7,56 @@ from plugin_evaluation.cockpit_management.cockpit import Cockpit
 from plugin_evaluation.exporter import Exporter
 from plugin_evaluation.settings import DATABASE_HOST, DATABASE_PORT
 
+workload_execution_time = 10.0
+
 print(intro)
 
+print("Starting cockpit...")
 cockpit = Cockpit()
 cockpit.start()
+print("   OK")
 
 sleep(3.0)
 
+print("Adding database...")
 cockpit.backend.add_database("momentum", DATABASE_HOST, DATABASE_PORT)
-print("Database added")
+print("   OK")
 
+print("Waiting default tables to load...")
 sleep(5.0)
+print("   OK")
+
+print("Starting a workload...")
+cockpit.backend.start_workload("tpch_0_1", 300)
+print("   OK")
 
 startts = time_ns()
 
-cockpit.backend.start_workload("tpch_0_1", 300)
-print("Workload started")
-
-sleep(30.0)
+print("Executing a workload...")
+sleep(workload_execution_time)
+print("   OK")
 
 endts = time_ns()
 sleep(1.0)
+
+print("Stopping a workload...")
 cockpit.backend.stop_workload("tpch_0_1")
-print("Workload stoped")
+print("   OK")
 
+print("Removing database...")
 cockpit.backend.remove_database("momentum")
-print("Workload removed")
+print("   OK")
 
-
-print(cockpit.get_stderr())
-
+print("Cockpit shutdown...")
 cockpit.shutdown()
-print("Cockpit shutdown")
+print("   OK")
 
 sleep(3.0)
 
-print(f"startts: {startts}")
-print(f"endts: {endts}")
-
+print("Export...")
 exporter = Exporter()
 exporter.plot_metric("throughput", "momentum", startts, endts)
 exporter.plot_metric("latency", "momentum", startts, endts)
 exporter.plot_metric("queue_length", "momentum", startts, endts)
 exporter.plot_metric("cpu_process_usage", "momentum", startts, endts)
+print("   OK")
