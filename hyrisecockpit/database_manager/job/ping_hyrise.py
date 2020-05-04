a4a4e919d9e681d9035c1b598011be6d97fac51f
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-05-04 14:42:31 +0200

    Adjust ping job

--- hyrisecockpit/database_manager/job/ping_hyrise.py
+++ hyrisecockpit/database_manager/job/ping_hyrise.py
@@ -3,10 +3,11 @@ from psycopg2 import DatabaseError, InterfaceError
 
 
 def ping_hyrise(connection_factory, hyrise_active) -> None:
-    """Check in interval if hyrise is still alive."""
+    """Check whether Hyrise is alive."""
     try:
         with connection_factory.create_cursor() as cur:
             cur.execute("SELECT 1;", None)
-            hyrise_active.value = True
     except (DatabaseError, InterfaceError):
         hyrise_active.value = False
+    else:
+        hyrise_active.value = True
