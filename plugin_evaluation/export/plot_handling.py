c8649c21667d637c8a272ae8ee672fa9f329946e
PeterTsayun <petr.tsayun@student.hpi.de>
2020-05-05 13:09:16 +0000

    Plot latency values for most expensive queries

--- plugin_evaluation/export/plot_handling.py
+++ plugin_evaluation/export/plot_handling.py
@@ -29,13 +29,16 @@ def plot_bar_chart(
     labels: List, metric_values: List, metric: str, x_label: str, y_label: str
 ):
     """Plot line chart to file."""
+    max_value = np.amax(metric_values)
     figure(num=None, figsize=(12, 6), dpi=80, facecolor="w", edgecolor="k")
     plt.title(f"{metric}")
     plt.ticklabel_format(style="plain")
     plt.xticks(rotation=45, ha="right")
     plt.subplots_adjust(bottom=0.15)
-    plt.ylim(bottom=0.0, top=np.amax(metric_values) * 1.3)
+    plt.ylim(bottom=0.0, top=max_value * 1.3)
     plt.bar(labels, metric_values, label=f"{metric}")
+    for index, value in enumerate(metric_values):
+        plt.text(index - 0.25, value + max_value * 0.02, ("%.1f" % value), rotation=45)
     plt.ylabel(f"{y_label}")
     plt.xlabel(f"{x_label}")
     plt.legend()
