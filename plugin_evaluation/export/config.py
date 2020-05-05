994650e75e2d277a1e5d2afc815c0d5216767332
PeterTsayun <petr.tsayun@student.hpi.de>
2020-05-05 08:30:02 +0000

    Don't fill with zero system data

--- plugin_evaluation/export/config.py
+++ plugin_evaluation/export/config.py
@@ -1,6 +1,9 @@
 """Module with metric configs."""
 
-from plugin_evaluation.export.influx_handling import get_metric_data_with_fill
+from plugin_evaluation.export.influx_handling import (
+    get_metric_data,
+    get_metric_data_with_fill,
+)
 from plugin_evaluation.export.metric_handling import idle_function, ns_to_ms
 
 config = {
@@ -19,13 +22,13 @@ config = {
     "queue_length": {
         "table_name": "queue_length",
         "label": "number of items",
-        "influx_function": get_metric_data_with_fill,
+        "influx_function": get_metric_data,
         "metric_function": idle_function,
     },
     "cpu_process_usage": {
         "table_name": "system_data",
         "label": "% usage",
-        "influx_function": get_metric_data_with_fill,
+        "influx_function": get_metric_data,
         "metric_function": idle_function,
     },
 }
--- plugin_evaluation/export/influx_handling.py
+++ plugin_evaluation/export/influx_handling.py
@@ -12,13 +12,34 @@ from plugin_evaluation.settings import (
 def get_metric_data_with_fill(
     table_name: str, metric: str, database: str, startts: int, endts: int
 ):
-    """Get metric data for provided time range."""
+    """Get metric data for provided time range, fill with 0 if points missed."""
     query: str = f"""SELECT MEAN({metric}) AS {metric}
         FROM {table_name}
         WHERE time >= $startts
         AND time < $endts
         GROUP BY TIME(1s)
         FILL(0.0)"""
+
+    client = InfluxDBClient(STORAGE_HOST, STORAGE_PORT, STORAGE_USER, STORAGE_PASSWORD)
+    result = client.query(
+        query,
+        database=database,
+        bind_params={"startts": startts, "endts": endts},
+        epoch=True,
+    )
+    client.close()
+
+    return list(result[table_name, None])
+
+
+def get_metric_data(
+    table_name: str, metric: str, database: str, startts: int, endts: int
+):
+    """Get metric data for provided time range."""
+    query: str = f"""SELECT {metric}
+        FROM {table_name}
+        WHERE time >= $startts
+        AND time < $endts"""
 
     client = InfluxDBClient(STORAGE_HOST, STORAGE_PORT, STORAGE_USER, STORAGE_PASSWORD)
     result = client.query(
