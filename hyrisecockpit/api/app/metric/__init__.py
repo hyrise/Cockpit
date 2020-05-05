8d622fec679819571211ac252afcd3968d2788fa
PeterTsayun <petr.tsayun@student.hpi.de>
2020-05-05 16:17:40 +0000

    Add metric module

---
+++ hyrisecockpit/api/app/metric/__init__.py
@@ -0,0 +1,16 @@
+"""Monitor entity.
+
+Should be split into multiple entities.
+"""
+
+from flask import Flask
+from flask_restx import Api
+
+BASE_ROUTE: str = "metric"
+
+
+def register_routes(api: Api, app: Flask, root: str) -> None:
+    """Register all monitor routes."""
+    from .app import api as metric_api
+
+    api.add_namespace(metric_api, path=f"{root}/{BASE_ROUTE}")
