9d3ac619b733479b2dbc0a14dac2dc59363fd634
PeterTsayun <petr.tsayun@student.hpi.de>
2020-05-05 16:42:31 +0000

    Add controller
    Co-authored-by: Alexander Dubrawski <dubrawski.alexander@googlemail.com>

---
+++ hyrisecockpit/api/app/metric/controller.py
@@ -0,0 +1,30 @@
+"""Controllers for metrics."""
+from flask import request
+from flask_accepts import accepts, responds
+from flask_restx import Namespace, Resource
+
+from .interface import TimeIntervalInterface
+from .schema import ThroughputSchema
+from .service import MetricService
+
+api = Namespace("Metric", description="Metric data.")
+
+
+@api.route("/throughput")
+class ThroughputController(Resource):
+    """Controller of Throughput data."""
+
+    @accepts(
+        dict(name="startts", type=int),  # noqa
+        dict(name="endts", type=int),  # noqa
+        dict(name="precision", type=int),  # noqa
+    )
+    @responds(schema=ThroughputSchema(many=True), api=api)
+    def get(self):
+        """Get throughput data for the requested time interval."""
+        interface: TimeIntervalInterface = TimeIntervalInterface(
+            startts=request.parsed_args["startts"],
+            endts=request.parsed_args["endts"],
+            precision=request.parsed_args["precision"],
+        )
+        return MetricService.get_throughput(interface)
