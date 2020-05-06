cda85dbb295882fd6930c9a6ed90bdfbe7230a07
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-05-06 20:24:09 +0200

    Add first tests for service

---
+++ tests/api/metric/test_metric_service.py
@@ -0,0 +1,87 @@
+"""Tests for the metric service."""
+
+from unittest.mock import patch
+
+from pytest import fixture
+
+from hyrisecockpit.api.app.metric.service import MetricService
+from hyrisecockpit.cross_platform_support.testing_support import MagicMock
+
+
+@fixture
+def metric_service() -> MetricService:
+    """Return metric service."""
+    return MetricService  # type: ignore
+
+
+class TestMetricService:
+    """Tests for the metric service."""
+
+    @patch("hyrisecockpit.api.app.metric.service.get_interval_limits")
+    @patch("hyrisecockpit.api.app.metric.service.get_historical_metric")
+    @patch("hyrisecockpit.api.app.metric.service.StorageConnection")
+    def test_get_data_for_time_intervall(
+        self,
+        mock_storage_connection: MagicMock,
+        mock_get_historical_metric: MagicMock,
+        mock_get_interval_limits: MagicMock,
+        metric_service: MetricService,
+    ) -> None:
+        """Test get data."""
+        fake_time_interval = {"startts": 42, "endts": 100, "precision": 1}
+        fake_table_name = "table_name"
+        fake_column_names = ["column_one", "column_two"]
+        mock_get_interval_limits.return_value = (
+            50,
+            100,
+        )
+        mock_get_historical_metric.return_value = "response"
+
+        mock_client: MagicMock = MagicMock()
+        mock_storage_connection.return_value.__enter__.return_value = mock_client
+
+        response = metric_service.get_data(
+            fake_time_interval, fake_table_name, fake_column_names  # type: ignore
+        )
+
+        mock_get_interval_limits.assert_called_once_with(42, 100, 1)
+        mock_get_historical_metric.assert_called_once_with(
+            50, 100, 1, fake_table_name, fake_column_names, mock_client
+        )
+        assert response == "response"
+
+    def test_get_throughput(self, metric_service: MetricService) -> None:
+        """Test get throughput."""
+        mock_get_data: MagicMock = MagicMock()
+        metric_service.get_data = mock_get_data  # type: ignore
+
+        fake_time_interval = "fake_interval"
+
+        metric_service.get_throughput(fake_time_interval)  # type: ignore
+        mock_get_data.assert_called_once_with(
+            fake_time_interval, "throughput", ["throughput"]
+        )
+
+    def test_get_latency(self, metric_service: MetricService) -> None:
+        """Test get latency."""
+        mock_get_data: MagicMock = MagicMock()
+        metric_service.get_data = mock_get_data  # type: ignore
+
+        fake_time_interval = "fake_interval"
+
+        metric_service.get_latency(fake_time_interval)  # type: ignore
+        mock_get_data.assert_called_once_with(
+            fake_time_interval, "latency", ["latency"]
+        )
+
+    def test_get_queue_length(self, metric_service: MetricService) -> None:
+        """Test get queue_length."""
+        mock_get_data: MagicMock = MagicMock()
+        metric_service.get_data = mock_get_data  # type: ignore
+
+        fake_time_interval = "fake_interval"
+
+        metric_service.get_queue_length(fake_time_interval)  # type: ignore
+        mock_get_data.assert_called_once_with(
+            fake_time_interval, "queue_length", ["queue_length"]
+        )
