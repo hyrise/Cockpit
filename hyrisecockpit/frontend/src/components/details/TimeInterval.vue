edfc324e599efc9d1bceb433cbebb5bd56c59d1e
monasobh <Mona.Sobhani@student.hpi.uni-potsdam.de>
2020-05-08 23:48:17 +0200

    change interval font to regular

--- hyrisecockpit/frontend/src/components/details/TimeInterval.vue
+++ hyrisecockpit/frontend/src/components/details/TimeInterval.vue
@@ -1,5 +1,5 @@
 <template>
-  <b class="interval secondary--text">{{ interval }}</b>
+  <div class="body-2 interval secondary--text">{{ interval }}</div>
 </template>
 
 <script lang="ts">
@@ -70,7 +70,6 @@ export function useUpdatingInterval(
 </script>
 <style scoped>
 .interval {
-  font-size: 14px;
   word-break: normal;
   line-height: normal;
 }
