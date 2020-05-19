0b97ba28bfe61b15c9ba74f8c0fb6977e741d1aa
Peter Tsayun <Petr.Tsayun@student.hpi.de>
2020-05-19 10:29:15 +0000

    Adjust update_krueger_Data job to work with the real data

--- hyrisecockpit/database_manager/job/update_krueger_data.py
+++ hyrisecockpit/database_manager/job/update_krueger_data.py
@@ -2,31 +2,56 @@
 
 from json import dumps
 from time import time_ns
+from typing import Dict, List
 
 from hyrisecockpit.database_manager.cursor import StorageConnectionFactory
+from hyrisecockpit.database_manager.job.sql_to_data_frame import sql_to_data_frame
 
 
-def update_krueger_data(storage_connection_factory: StorageConnectionFactory) -> None:
+def update_krueger_data(
+    database_blocked,
+    connection_factory,
+    storage_connection_factory: StorageConnectionFactory,
+) -> None:
     """Update krueger data."""
     time_stamp = time_ns()
-    executed_mocked_data = {
-        "SELECT": 100,
-        "INSERT": 0,
-        "UPDATE": 0,
-        "DELETE": 0,
+    sql = """WITH query_latency AS (SELECT SUM(walltime_ns) AS latency, query_hash
+        FROM meta_cached_operators
+        GROUP BY query_hash)
+        SELECT hash_value, latency, frequency, sql_string FROM query_latency JOIN meta_cached_queries
+        ON query_latency.query_hash = meta_cached_queries.hash_value;"""
+
+    meta_segments = sql_to_data_frame(database_blocked, connection_factory, sql, None)
+
+    counts: Dict = {  # (total_latency, total_frequency)
+        "SELECT": (0, 0),
+        "CREATE": (0, 0),
+        "UPDATE": (0, 0),
+        "INSERT": (0, 0),
+        "DELETE": (0, 0),
+        "COPY": (0, 0),
     }
-    generated_mocked_data = {
-        "SELECT": 100,
-        "INSERT": 0,
-        "UPDATE": 0,
-        "DELETE": 0,
-    }
+
+    if not meta_segments.empty:
+        for _, row in meta_segments.iterrows():
+            for query_type in counts.keys():
+                if row["sql_string"].startswith(query_type):
+                    counts[query_type] = (
+                        counts[query_type][0] + row["latency"],
+                        counts[query_type][1] + row["frequency"],
+                    )
+                    break
+
+    krueger_data: List = [
+        {
+            "query_type": query_type,
+            "total_latency": total_latency,
+            "total_frequency": total_frequency,
+        }
+        for query_type, (total_latency, total_frequency) in counts.items()
+    ]
+
     with storage_connection_factory.create_cursor() as log:
         log.log_meta_information(
-            "krueger_data",
-            {
-                "executed": dumps(executed_mocked_data),
-                "generated": dumps(generated_mocked_data),
-            },
-            time_stamp,
+            "krueger_data", {"krueger_data": dumps(krueger_data)}, time_stamp,
         )
