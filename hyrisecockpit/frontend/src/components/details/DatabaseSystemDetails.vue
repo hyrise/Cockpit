ef2f63e9e669423bdb3918fee8a1bf18ab991899
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2020-05-25 14:24:02 +0200

    add costum font sizing

--- hyrisecockpit/frontend/src/components/details/DatabaseSystemDetails.vue
+++ hyrisecockpit/frontend/src/components/details/DatabaseSystemDetails.vue
@@ -8,14 +8,7 @@
     >
       <v-card class="card" :color="database.color">
         <v-row class="detailsRow">
-          <v-col
-            id="database-host"
-            :lg="databases.length > 1 ? 4 : 3"
-            xl="3"
-            :class="{
-              multirowCol: !$vuetify.breakpoint.xl && databases.length > 1,
-            }"
-          >
+          <v-col id="database-host" col="2" class="mr-1">
             <v-card-title class="font-weight-regular white--text noPadding">
               {{ database.systemDetails.host }}
             </v-card-title>
@@ -23,29 +16,15 @@
               >Host</v-card-text
             >
           </v-col>
-          <v-col
-            id="database-memory-capacity"
-            :lg="databases.length > 1 ? 4 : 2"
-            xl="2"
-            :class="{
-              multirowCol: !$vuetify.breakpoint.xl && databases.length > 1,
-            }"
-          >
+          <v-col id="database-memory-capacity" col="2" class="mr-1">
             <v-card-title class="font-weight-light white--text noPadding"
-              >{{ database.systemDetails.memoryCapacity }} GB</v-card-title
+              >{{ database.systemDetails.memoryCapacity }}&nbsp;GB</v-card-title
             >
             <v-card-text class="font-weight-light white--text noPadding"
               >Memory Capacity</v-card-text
             >
           </v-col>
-          <v-col
-            id="database-number-cpus"
-            :lg="databases.length > 1 ? 4 : 2"
-            xl="2"
-            :class="{
-              multirowCol: !$vuetify.breakpoint.xl && databases.length > 1,
-            }"
-          >
+          <v-col id="database-number-cpus" col="2" class="mr-1">
             <v-card-title class="font-weight-light white--text noPadding">
               {{ database.systemDetails.numberOfCPUs }}
             </v-card-title>
@@ -53,30 +32,17 @@
               ># of CPUs</v-card-text
             >
           </v-col>
-          <v-col xl="0" :lg="databases.length > 1 ? 4 : 0" />
-          <v-col
-            id="database-memory-footprint"
-            :lg="databases.length > 1 ? 4 : 2"
-            xl="2"
-            :class="{
-              multirowCol: !$vuetify.breakpoint.xl && databases.length > 1,
-            }"
-          >
+          <v-col id="database-memory-footprint" col="2" class="mr-1">
             <v-card-title class="mr-6 font-weight-light white--text noPadding"
-              >{{ database.systemDetails.memoryFootprint }} MB</v-card-title
+              >{{
+                database.systemDetails.memoryFootprint
+              }}&nbsp;MB</v-card-title
             >
             <v-card-text class="mr-10 font-weight-light white--text noPadding"
               >Data Size</v-card-text
             >
           </v-col>
-          <v-col
-            id="database-number-workers"
-            :lg="databases.length > 1 ? 4 : 2"
-            xl="2"
-            :class="{
-              multirowCol: !$vuetify.breakpoint.xl && databases.length > 1,
-            }"
-          >
+          <v-col id="database-number-workers" col="2">
             <v-card-title class="font-weight-light white--text noPadding">
               {{ database.systemDetails.numberOfWorkers }}
             </v-card-title>
@@ -157,16 +123,25 @@ export default defineComponent({
 }
 .v-card__title {
   padding-bottom: 6px;
+  white-space: nowrap;
+}
+
+@media only screen and (max-width: 1600px) {
+  .v-card__title {
+    padding-bottom: 6px;
+    white-space: nowrap;
+    font-size: 1rem !important;
+  }
+  .v-card__text {
+    font-size: 0.75rem !important;
+  }
 }
 .noPadding {
   padding: 0px !important;
   word-break: normal !important;
 }
+
 .detailsRow {
   padding: 12px;
-}
-.multirowCol {
-  margin-bottom: 8px;
-  font-size: 1em !important;
 }
 </style>
