a635d7600a9378d91895a91b1d74a854d4d4e0a8
fabianhe <fabian.heseding@web.de>
2020-05-27 15:12:04 +0200

    Remove control namespace

--- hyrisecockpit/api/app/routes.py
+++ hyrisecockpit/api/app/routes.py
@@ -6,14 +6,12 @@ from flask_restx import Api
 
 def register_routes(api: Api, app: Flask, root: str = "/api") -> None:
     """Register all sub-routes."""
-    from .control import register_routes as attach_control
     from .database import register_routes as attach_database
     from .monitor import register_routes as attach_monitor
     from .plugin import register_routes as attach_plugin
     from .sql import register_routes as attach_sql
     from .workload import register_routes as attach_workload
 
-    attach_control(api, app, root)
     attach_database(api, app, root)
     attach_monitor(api, app, root)
     attach_plugin(api, app, root)
