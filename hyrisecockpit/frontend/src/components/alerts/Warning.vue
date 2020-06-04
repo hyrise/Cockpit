1062bd5158c401d123ba62d30d336ea172e01426
monasobh <49497676+monasobh@users.noreply.github.com>
2020-06-04 11:44:57 +0200

    add review changes

--- hyrisecockpit/frontend/src/components/alerts/Warning.vue
+++ hyrisecockpit/frontend/src/components/alerts/Warning.vue
@@ -10,25 +10,24 @@
     <v-icon :color="colorValueDefinition.darkgrey" size="27" class="mr-3">
       mdi-alert-outline
     </v-icon>
-
     <slot class="mt-6" name="message" />
-
     <v-btn :color="colorValueDefinition.darkgrey" icon @click="banner = false">
       <v-icon size="20">mdi-close</v-icon>
     </v-btn>
-  </v-banner> </template
->y
+  </v-banner>
+</template>
 <script lang="ts">
 import {
   defineComponent,
   SetupContext,
   computed,
   Ref,
+  ref,
 } from "@vue/composition-api";
 import { colorValueDefinition } from "../../meta/colors";
 
 interface Data {
-  banner: boolean;
+  banner: Ref<boolean>;
   colorValueDefinition: Record<string, string>;
   show: Ref<boolean>;
 }
@@ -46,9 +45,10 @@ export default defineComponent({
     },
   },
   setup(props: Props, context: SetupContext): Data {
+    const banner = ref<boolean>(true);
     return {
       colorValueDefinition,
-      banner: true,
+      banner,
       show: computed(() => !props.condition.length),
     };
   },
