188f1729883499b3620a7ef4fd4e7f51dcd841dc
Peter Tsayun <Petr.Tsayun@student.hpi.de>
2020-07-15 14:06:49 +0000

    Adjust plugin log

--- hyrisecockpit/database_manager/job/update_plugin_log.py
+++ hyrisecockpit/database_manager/job/update_plugin_log.py
@@ -1,17 +1,9 @@
 """This job updates the plug-in log."""
 
-from datetime import datetime
-from time import time
+from time import time_ns
 
 from hyrisecockpit.database_manager.cursor import StorageConnectionFactory
 from hyrisecockpit.database_manager.job.sql_to_data_frame import sql_to_data_frame
-
-
-def _datetime_str_to_unix_timestamp(datetime_str: str) -> int:
-    """Convert timestamp of '%Y-%m-%d %H:%M:%S' format to Unix timestamp with millisecond precision."""
-    date = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
-    unix_timestamp = int(date.timestamp() * 1_000)
-    return unix_timestamp
 
 
 def update_plugin_log(
@@ -20,10 +12,10 @@ def update_plugin_log(
     storage_connection_factory: StorageConnectionFactory,
 ) -> None:
     """Update plugin log."""
-    offset_sec = 5.0
-    timestamp = time()
-    startts = str(datetime.fromtimestamp(timestamp - offset_sec))
-    endts = str(datetime.fromtimestamp(timestamp))
+    offset_ns = 5_000_000_000
+    timestamp = time_ns()
+    startts = timestamp - offset_ns
+    endts = timestamp
 
     log_df = sql_to_data_frame(
         database_blocked,
@@ -36,12 +28,7 @@ def update_plugin_log(
         return
 
     plugin_log = [
-        (
-            _datetime_str_to_unix_timestamp(row["timestamp"]),
-            row["reporter"],
-            row["message"],
-            row["log_level"],
-        )
+        (row["timestamp"], row["reporter"], row["message"], row["log_level"],)
         for row in log_df.to_dict("index").values()
     ]
 
