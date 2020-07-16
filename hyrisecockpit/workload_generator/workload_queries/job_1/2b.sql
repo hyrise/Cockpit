87ce24dff7817a355c3e930ceca9ba28983a233e
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-07-16 18:28:42 +0200

    Remove duplicated files

--- hyrisecockpit/workload_generator/workload_queries/job_1/2b.sql
+++
@@ -1,9 +0,0 @@
-SELECT MIN(t.title) AS movie_title
-FROM company_name_job_1 AS cn, keyword_job_1 AS k, movie_companies_job_1 AS mc, movie_keyword_job_1 AS mk, title_job_1 AS t
-WHERE cn.country_code = '[nl]'
-		AND k.keyword = 'character-name-in-title'
-		AND cn.id = mc.company_id
-		AND mc.movie_id = t.id
-		AND t.id = mk.movie_id
-		AND mk.keyword_id = k.id
-		AND mc.movie_id = mk.movie_id;
--- hyrisecockpit/workload_generator/workload_queries/job_1/3b.sql
+++
@@ -1,9 +0,0 @@
-SELECT MIN(t.title) AS movie_title
-FROM keyword_job_1 AS k, movie_info_job_1 AS mi, movie_keyword_job_1 AS mk, title_job_1 AS t
-WHERE k.keyword LIKE '%sequel%'
-		AND mi.info IN ('Bulgaria')
-		AND t.production_year > 2010
-		AND t.id = mi.movie_id
-		AND t.id = mk.movie_id
-		AND mk.movie_id = mi.movie_id
-		AND k.id = mk.keyword_id;
