6313ae7c33f354097c6645838d1111999aa2fc2e
kathwill <katharina.wille@student.hpi.de>
2020-07-30 19:43:08 +0200

    change string to template string

--- hyrisecockpit/frontend/src/meta/names.ts
+++ hyrisecockpit/frontend/src/meta/names.ts
@@ -4,16 +4,7 @@ export function changeTableName(table: string): string {
   if (isInTestMode) return table;
   const words = table.split("_");
   if (words.length === 4) {
-    return (
-      words[0] +
-      " (" +
-      words[1].toUpperCase() +
-      " SF " +
-      words[2] +
-      "." +
-      words[3] +
-      ")"
-    );
+    return `${words[0]} (${words[1].toUpperCase()} SF ${words[2]}.${words[3]})`;
   }
-  return words[0] + " (" + words[1].toUpperCase() + " SF " + words[2] + ")";
+  return `${words[0]} (${words[1].toUpperCase()} SF ${words[2]})`;
 }
