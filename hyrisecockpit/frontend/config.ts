35eea9de86ef63b93b8b1a9e3f15e0faa3dc1ea2
schTi <49444254+schTi@users.noreply.github.com>
2020-08-03 08:52:02 +0200

    fix host (#734)

--- hyrisecockpit/frontend/config.ts
+++ hyrisecockpit/frontend/config.ts
@@ -3,7 +3,7 @@ const devServerUrl = "http://127.0.0.1:3000/";
 //const backendUrl = devServerUrl;
 const backendUrl = (window as any).Cypress?.env("stubless")
   ? devServerUrl
-  : `http://${process.env.VUE_APP_BACKEND_HOST || "backend"}:${
+  : `http://${process.env.VUE_APP_BACKEND_HOST || "localhost"}:${
       process.env.VUE_APP_BACKEND_PORT || "8000"
     }/`;
 
