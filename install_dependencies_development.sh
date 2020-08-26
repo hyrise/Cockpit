962d2812d310eda7d0eec26754a43119e403e9b0
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-08-26 17:35:10 +0200

    Add install dependencies script

---
+++ install_dependencies_development.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+unamestr=$(uname)
+if [[ "$unamestr" == 'Darwin' ]]; then
+    echo "Installing dependencies (this may take a while)..."
+    for formula in pyenv libpq postgresql node; do
+        if brew ls --versions $formula > /dev/null; then
+            echo "$formula already installed"
+            continue
+        elif ! brew install $formula; then
+            echo "Error during brew formula $formula installation."
+            exit 1
+        fi
+    done
+
+    if ! pyenv versions | grep '3.8.5'; then
+        echo "Install Python 3.8.5 for pyenv."
+        pyenv install 3.8.5
+    fi
+
+    pyenv local 3.8.5
+    python -m pip install pipenv
+    exec "$SHELL"
+    pipenv --three --python=`python --version`
+    pipenv sync --dev
+    pipenv run pre-commit install
+    cd hyrisecockpit/frontend && npm install
+
+elif [[ "$unamestr" == 'Linux' ]]; then
+    echo "Installing dependencies (this may take a while)..."
+    sudo apt-get update
+    sudo apt-get install -y \
+            python3-pip \
+            make \
+            build-essential \
+            libssl-dev \
+            zlib1g-dev \
+            libbz2-dev \
+            libreadline-dev \
+            libsqlite3-dev \
+            wget \
+            curl \
+            llvm \
+            libncurses5-dev \
+            libncursesw5-dev \
+            xz-utils \
+            tk-dev \
+            libffi-dev \
+            liblzma-dev \
+            python-openssl \
+            git 
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    echo -e "export PATH=\"/home/\$USER/.pyenv/bin:\$PATH\"
+    eval \"\$(pyenv init -)\"
+    eval \"\$(pyenv virtualenv-init -)\"\n" >> .bashrc
+
+    PYENV_ROOT=$HOME/.pyenv
+    PATH=$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH
+    pyenv install -y  3.8.5
+
+    pip install --upgrade pip
+    pip install pipenv
+    pipenv sync --dev
+    pipenv run pre-commit install
+
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    cd hyrisecockpit/frontend && npm install
+fi
