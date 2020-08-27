1a8f51540b99774ba7b0ad0f4267e83844df62c7
Alexander Dubrawski <dubrawski.alexander@googlemail.com>
2020-08-27 19:25:11 +0200

    Improve docker setup (#747)
    * Add start script
    
    * Adjust docker file
    
    * Remove unneeded docker files
    
    * simplify docker compose
    
    * Adjust dockerfiles
    
    * Adjust hyrise docker file
    
    * Adjust git hub action
    
    * Adjust env for browser
    
    * Adjust settings
    
    * Adjust to load tables
    
    * Update Hyrise docker file
    
    * Adjust docker file
    
    * Adjust usage ov docker env
    
    * Add push action
    
    * fix action
    
    * Change ADD to COPY
    
    * Use tar to extract tables and adjust number threads
    
    * Adjust module description
    
    * Add watchtower and adjust tags
    
    * Adjusr env to work with ubuntu
    
    * Add deployment docker compose
    
    * Fix typo
    
    * Adjust docker compose and remove watchtower
    
    * Fix ports
    
    * Fix docker file
    
    * Reduce to one compose file and remove deployment action
    
    * Remove hooks and adjust dockerfile
    
    * Improve readability
    
    * Improve dockerfile
    
    * Fix docker file
    
    * Adjust dropbox url
    
    * Make docker compose more robust
    
    * Adjust docker file
    
    * set debug to false
    
    * Implement feedback
    
    * Add shebang
    
    * Fix docker file
    
    * Adjust dockerfile to build binary tables
    
    * Add MANIFEST to package additional files
    
    * Adjust comment
    
    * Update dockerfile
    
    * Adjust to use less memory
    
    * Fix docker file
    
    * Adjust threads and dict name
    
    * Reduce code duplication

--- hyrisecockpit/settings.py
+++ hyrisecockpit/settings.py
@@ -20,24 +20,24 @@ BACKEND_HOST: str = getenv("BACKEND_HOST", "127.0.0.1")
 BACKEND_PORT: str = getenv("BACKEND_PORT", "8000")
 BACKEND_LISTENING: str = getenv("BACKEND_LISTENING", "0.0.0.0")
 
-DB_MANAGER_HOST: str = getenv("DB_MANAGER_HOST", "manager")
+DB_MANAGER_HOST: str = getenv("DB_MANAGER_HOST", "127.0.0.1")
 DB_MANAGER_PORT: str = getenv("DB_MANAGER_PORT", "8001")
 DB_MANAGER_LISTENING: str = getenv("DB_MANAGER_LISTENING", "*")
 
-GENERATOR_HOST: str = getenv("GENERATOR_HOST", "generator")
+GENERATOR_HOST: str = getenv("GENERATOR_HOST", "127.0.0.1")
 GENERATOR_PORT: str = getenv("GENERATOR_PORT", "8002")
 GENERATOR_LISTENING: str = getenv("GENERATOR_LISTENING", "*")
 
-WORKLOAD_SUB_HOST: str = getenv("WORKLOAD_SUB_HOST", "generator")
+WORKLOAD_SUB_HOST: str = getenv("WORKLOAD_SUB_HOST", "127.0.0.1")
 WORKLOAD_PUBSUB_PORT: str = getenv("WORKLOAD_PUBSUB_PORT", "8003")
 WORKLOAD_LISTENING: str = getenv("WORKLOAD_LISTENING", "*")
 
 DEFAULT_TABLES: str = getenv("DEFAULT_TABLES", "tpch_0_1")
 
-STORAGE_HOST: str = getenv("STORAGE_HOST", "influxdb")
+STORAGE_HOST: str = getenv("STORAGE_HOST", "127.0.0.1")
 STORAGE_PORT: str = getenv("STORAGE_PORT", "8086")
 STORAGE_USER: str = getenv("STORAGE_USER", "root")
 STORAGE_PASSWORD: str = getenv("STORAGE_PASSWORD", "root")
 
 FLASK_ENV: str = getenv("FLASK_ENV", "development")
-FLASK_DEBUG: bool = bool(getenv("FLASK_DEBUG", True))
+FLASK_DEBUG: bool = bool(getenv("FLASK_DEBUG", False))
