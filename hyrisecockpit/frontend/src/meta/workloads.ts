4ffad0c345e3517d46585c0204eb0e1b98a59e93
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-09-25 18:58:09 +0200

    Adjust workload name extraction

--- hyrisecockpit/frontend/src/meta/workloads.ts
+++ hyrisecockpit/frontend/src/meta/workloads.ts
@@ -1,16 +1,45 @@
 import { isInTestMode } from "../../config";
 
+function getDisplayWorkloadName(
+  workloadType: string
+): string {
+  const workload_type_map: Record<string, string> = {
+    "tpch": "TPC-H",
+    "tpcds": "TPC-DS",
+    "tpcc": "TPC-C",
+    "job": "Join Order Benchmark"
+  }
+  return workload_type_map[workloadType];
+}
+
 export function getWorkloadName(
   workloadType: string,
   scaleFactor: string
 ): string {
-  return `${workloadType.toUpperCase()} (SF ${scaleFactor})`;
+  return `${getDisplayWorkloadName(workloadType)} (SF ${scaleFactor})`;
 }
+
+// The following code is pretty ugly. The workload selector component is working somehow
+// with the workload name. So every time a workload is selected from the workload name (displayed name)
+// the name and scale factor needs to be retrieved. If for example the *name: job, scalefactor: 1* is transformed to
+// *Join Order Benchmark (SF 1)* we need to extract job *name: job, scalefactor: 1*. For that we are using regex.
+
 export function getWorkloadType(workloadName: string): string {
-  return workloadName.split(" ")[0].toLowerCase();
+  const workload_type_map: Record<string, string> = {
+    "TPC-H": "tpch",
+    "TPC-DS": "tpcds",
+    "TPC-C": "tpcc",
+    "Join Order Benchmark": "job"
+  }
+  const workload: string =  workloadName.split(" (")[0];
+  return workload_type_map[workload]
 }
+
 export function getScaleFactor(workloadName: string): string {
-  return workloadName.split(" ")[2];
+  const regExp = /\(([^)]+)\)/;
+  const matches = regExp.exec(workloadName)
+  // @ts-ignore
+  return matches[1].split(" ")[1]
 }
 
 export function getTableName(table: string): string {
