74fef69c61ad988e397a25bd0db1364bd560c044
Alexander Dubrawski <dubrawski.alexander@googlemail.com>
2020-10-06 14:42:36 +0200

    B/improve backend system tests (#758)
    * Add pytest package
    
    * Add first draft docker backend system test
    
    * Adjust files
    
    * Add native cockpit system test
    
    * Add logging of container output
    
    * Add logging to native system test
    
    * Remove comment
    
    * Fix isort hook
    
    * Improve dockstrings
    
    * Delete readme
    
    * Install pytest-env package
    
    * Implement feedback

--- system_tests/test_backend.py
+++ system_tests/test_backend.py
@@ -1,63 +1,29 @@
-"""Module for system tests."""
+"""Module for backend system tests.
+
+The system test is defined through the following user scenario:
+1. Add a database
+2. Load TPC-H sf 0.1 tables
+3. Start workload generation for TPC-H (SF 0.1)
+4. Start workers
+5. Activate compression plug-in
+6. Change compression plug-in settings
+7. Get plug-in log
+8. deactivate plug-in
+9. Stop workload generation
+10. Stop workers
+11. Remove database
+"""
+
 from time import sleep, time_ns
 
-from influxdb import InfluxDBClient
-from system_tests.cockpit_backend import CockpitBackend
-from system_tests.cockpit_generator import CockpitGenerator
-from system_tests.cockpit_manager import CockpitManager
-from system_tests.settings import (
-    DATABASE_HOST,
-    DATABASE_PORT,
-    STORAGE_HOST,
-    STORAGE_PASSWORD,
-    STORAGE_PORT,
-    STORAGE_USER,
-)
-
-SETUP_TIMEOUT = 3.0
+from system_tests.settings import DATABASE_HOST, DATABASE_PORT, DEFAULT_TIME_OUT
 
 
 class TestSystem:
-    """Tests for the whole cockpit system."""
+    """This class tests every step of the user scenario."""
 
     @classmethod
-    def setup_class(cls):
-        """Run before every test."""
-        cls.influx_client = InfluxDBClient(
-            STORAGE_HOST, STORAGE_PORT, STORAGE_USER, STORAGE_PASSWORD
-        )
-        cls.clean_influx()
-        cls.backend = CockpitBackend()
-        cls.backend.start()
-        cls.manager = CockpitManager()
-        cls.manager.start()
-        cls.generator = CockpitGenerator()
-        cls.generator.start()
-        sleep(SETUP_TIMEOUT)
-
-    @classmethod
-    def teardown_class(cls):
-        """Run after every test."""
-        cls.manager.shutdown()
-        cls.backend.shutdown()
-        cls.generator.shutdown()
-        cls.clean_influx()
-        cls.influx_client.close()
-
-    @classmethod
-    def check_stderr(cls):
-        """Check standard error output of the components."""
-        assert cls.generator.get_stderr() == ""  # nosec
-        assert cls.backend.get_stderr() == ""  # nosec
-        assert cls.manager.get_stderr() == ""  # nosec
-
-    @classmethod
-    def clean_influx(cls):
-        """Delete Influx databases which were created while executing of the tests."""
-        cls.influx_client.drop_database("test_database1")
-
-    @classmethod
-    def check_tables_loading(
+    def check_tables_loaded(
         cls, database_id: str, workload_type: str, scalefactor: float
     ) -> None:
         """Check if database has successfully load tables."""
@@ -83,10 +49,6 @@ class TestSystem:
 
         assert benchmark_status["missing_tables"] == []  # nosec
         assert benchmark_status["completely_loaded"]  # nosec
-
-    def test_initializes_backend(self):
-        """Test backend initializes without errors."""
-        self.check_stderr()
 
     def test_returns_metric_values(self):
         """Test static metric endpoints return correct values."""
@@ -124,7 +86,7 @@ class TestSystem:
             assert response.json() == []  # nosec
 
     def test_returns_available_databases(self):
-        """Ensure a new backend has no databases."""
+        """Ensure a new started backend has no databases."""
         response = self.backend.get_property("control/database")
 
         assert response.status_code == 200  # nosec
@@ -145,19 +107,32 @@ class TestSystem:
             ]
         }
 
-    def test_initialization_calls(self):
-        """Test initialization calls had no errors."""
-        self.check_stderr()
-
     def test_adds_database(self):
-        """Test adding a database."""
+        """Test adding a database.
+
+        This test is adding a database with the id test_database1.
+        """
         response = self.backend.add_database(
             "test_database1", DATABASE_HOST, DATABASE_PORT
         )
         assert response.status_code == 200  # nosec
 
+    def test_creates_influx_database(self):
+        """Test the corresponding influx database is created.
+
+        For every added Hyrise database a corresponding database inside the influx
+        is created. So for example if we add a Hyrise with the id test_database1 we expect
+        that inside the influx is a corresponding database with the same name.
+        """
+        influx_databases = self.influx_client.get_list_database()
+        assert {"name": "test_database1"} in influx_databases  # nosec
+
     def test_added_database_is_in_available_databases(self):
-        """Test added database is in available databases."""
+        """Test added database is in available databases.
+
+        Here we test that the database we added before is returned
+        from the control/database endpoint.
+        """
         response = self.backend.get_property("control/database")
 
         assert response.status_code == 200  # nosec
@@ -166,44 +141,51 @@ class TestSystem:
                 "id": "test_database1",
                 "host": DATABASE_HOST,
                 "port": DATABASE_PORT,
-                "number_workers": 8,
+                "number_workers": 2,
                 "dbname": "postgres",
             }
         ]
 
-    def test_loads_default_tables(self):
-        """Test loading default tables."""
+    def test_loads_tpch_sf_0_1_tables(self):
+        """Test loading TPC-H tables with scale factor 0.1."""
         response = self.backend.load_tables("tpch", 0.1)
         assert response.status_code == 200  # nosec
 
-    def test_loading_completed(self):
-        """Test complete loading of the default tables."""
-        sleep(5.0)  # wait until default tables are loaded
-        self.check_tables_loading("test_database1", "tpch", 0.1)
+    def test_loading_tpch_sf_0_1_tables_completed(self):
+        """Test complete loading of the default tables.
 
-    def test_creates_influx_database(self):
-        """Test the corresponding influx database is created."""
-        influx_databases = self.influx_client.get_list_database()
-        assert {"name": "test_database1"} in influx_databases  # nosec
-
-    def test_handles_database_without_errors(self):
-        """Test database handling had no errors."""
-        self.check_stderr()
+        After a wait we check if the tables are completely loaded in the back-end.
+        """
+        sleep(DEFAULT_TIME_OUT)
+        self.check_tables_loaded("test_database1", "tpch", 0.1)
 
     def test_starts_workload_generator(self):
-        """Test starting of the workload generator."""
-        response = self.backend.start_workload("tpch", 0.1, 300)
+        """Test starting of the workload generator.
+
+        We start the workload generator. It will generate a TPC-H
+        workload with the scale factor 0.1. It will publish 10 tasks
+        per second.
+        """
+        response = self.backend.start_workload("tpch", 0.1, 10)
         assert response.status_code == 200  # nosec
 
     def test_starts_worker_pool(self):
-        """Test starting of the worker pool."""
+        """Test starting of the worker pool.
+
+        This tests starts the worker. As a result the Hyrise instance is
+        now under load.
+        """
         response = self.backend.start_workers()
         assert response.status_code == 200  # nosec
 
     def test_returns_historical_metric_values_during_workload_execution(self):
-        """Test responses of the historical metrics."""
-        sleep(4.0)  # wait for query executions
+        """Test responses of the historical metrics.
 
+        After a while we check the metric data for throughput, latency
+        and queue_length. With only 10 task per second it is possible that
+        the queue_length value is 0.
+        """
+        sleep(DEFAULT_TIME_OUT)  # wait for query executions
         metrics = ["metric/throughput", "metric/latency", "metric/queue_length"]
         attributes = ["throughput", "latency", "queue_length"]
         for metric, attribute in zip(metrics, attributes):
@@ -216,23 +198,39 @@ class TestSystem:
             )
 
             assert response.status_code == 200  # nosec
-            assert response.json()[0][attribute][0][attribute] > 0  # nosec
+            if attribute == "queue_length":
+                assert response.json()[0][attribute][0][attribute] >= 0.0  # nosec
+            else:
+                assert response.json()[0][attribute][0][attribute] > 0.0  # nosec
 
     def test_activates_plugin(self):
-        """Test activation of the plugin."""
+        """Test activation of the plug-in.
+
+        This tests activates the Compression plug-in.
+        """
         response = self.backend.activate_plugin("test_database1", "Compression")
 
         assert response.status_code == 200  # nosec
 
     def test_do_not_activates_already_activated_plugin(self):
-        """Test activation of the already activated plugin."""
-        sleep(1.0)
+        """Test activation of the already activated plug-in.
+
+        If we activated a plug-in in the Hyrise we need to check that if
+        we try to activate it again the back-end denies the activation.
+        This test checks that behavior by trying to activate the Compression plug-in
+        again.
+        """
+        sleep(DEFAULT_TIME_OUT)
         response = self.backend.activate_plugin("test_database1", "Compression")
 
         assert response.status_code == 423  # nosec
 
     def test_returns_activated_plugins(self):
-        """Test activation of the plugin."""
+        """Test activation of the plug-in.
+
+        THis tests checks if the Compression plug-in is returned correctly
+        inside all activated plug-ins.
+        """
         response = self.backend.get_activated_plugins()
         assert response.status_code == 200  # nosec
         assert response.json() == [  # nosec
@@ -254,15 +252,22 @@ class TestSystem:
         ]
 
     def test_sets_plugin_settings(self):
-        """Test set plugin settings."""
+        """Test set plug-in settings.
+
+        This test sets the plug-in settings for the Compression plug-in.
+        """
         response = self.backend.set_plugin_settings(
             "test_database1", "Compression", "MemoryBudget", "50000"
         )
         assert response.status_code == 200  # nosec
 
     def test_returns_new_plugin_settings(self):
-        """Test new plugin settings."""
-        sleep(1.0)
+        """Test new plug-in settings.
+
+        This test checks if the settings from the Compression plug-in
+        were set correctly.
+        """
+        sleep(DEFAULT_TIME_OUT)
         response = self.backend.get_activated_plugins()
         assert response.status_code == 200  # nosec
         assert response.json() == [  # nosec
@@ -284,8 +289,12 @@ class TestSystem:
         ]
 
     def test_returns_plugin_log(self):
-        """Test plugin log."""
-        sleep(1.0)
+        """Test plugin log.
+
+        This test checks if there is a plug-in log for the Compression plug-in
+        that is activated on test_database1.
+        """
+        sleep(DEFAULT_TIME_OUT)
         response = self.backend.get_plugin_log()
 
         assert response.json()[0]["id"] == "test_database1"  # nosec
@@ -294,14 +303,16 @@ class TestSystem:
         assert response.json()[0]["log"][0]["message"] == "Initialized!"  # nosec
 
     def test_deactivates_plugin(self):
-        """Test deactivation of the plugin."""
-        response = self.backend.deactivate_plugin("test_database1", "Compression")
+        """Test deactivation of the plugin.
 
+        This test deactivates the Compression plug-in.
+        """
+        response = self.backend.deactivate_plugin("test_database1", "Compression")
         assert response.status_code == 200  # nosec
 
     def test_deactivated_plugin_is_not_in_active_plugins(self):
         """Test deactivated plugin is not in the active plugins."""
-        sleep(1.0)
+        sleep(DEFAULT_TIME_OUT)
         response = self.backend.get_activated_plugins()
         assert response.status_code == 200  # nosec
         assert response.json() == [{"id": "test_database1", "plugins": []}]  # nosec
@@ -341,10 +352,6 @@ class TestSystem:
         """Test removing of the database."""
         response = self.backend.remove_database("test_database1")
         assert response.status_code == 200  # nosec
-
-    def test_executes_workload(self):
-        """Test workload execution had no errors."""
-        self.check_stderr()
 
     def test_keeps_influx_database(self):
         """Test the corresponding influx database is kept after database removing."""
