fcf77f7a767a6ef182203c3255ac353a83ce82d4
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-10-12 10:50:24 +0200

    Add models for system data

--- hyrisecockpit/api/app/monitor/model.py
+++ hyrisecockpit/api/app/monitor/model.py
@@ -7,8 +7,6 @@ from typing import List
 
 
 class FailedTaskEntry:
-    """Model of a failed task."""
-
     def __init__(self, timestamp: int, worker_id: str, task: str, error: str):
         self.timestamp: int = timestamp
         self.worker_id: str = worker_id
@@ -17,18 +15,49 @@ class FailedTaskEntry:
 
 
 class FailedTasks:
-    """Model of a failed task."""
-
     def __init__(self, database_id: str, failed_task_entries: List[FailedTaskEntry]):
         self.id: str = database_id
         self.failed_task_entries: List[FailedTaskEntry] = failed_task_entries
 
 
 class TimeInterval:
-    """Model of a time interval."""
-
     def __init__(self, startts: int, endts: int, precision: int):
-        """Initialize a Time Interval model."""
         self.startts: int = startts
         self.endts: int = endts
         self.precision: int = precision
+
+
+class Cpu:
+    def __init__(
+        self, cpu_system_usage: float, cpu_process_usage: float, cpu_count: int
+    ):
+        self.cpu_system_usage: float = cpu_system_usage
+        self.cpu_process_usage: float = cpu_process_usage
+        self.cpu_count: int = cpu_count
+
+
+class Memory:
+    def __init__(self, free: int, available: int, total: int, percent: float):
+        self.free: int = free
+        self.available: int = available
+        self.total: int = total
+        self.percent: float = percent
+
+
+class SystemDataEntry:
+    def __init__(self, cpu: Cpu, memory: Memory, database_threads: int):
+        self.cpu: Cpu = cpu
+        self.memory: Memory = memory
+        self.database_threads: int = database_threads
+
+
+class SystemData:
+    def __init__(self, timestamp: int, system_data_entry: SystemDataEntry):
+        self.timestamp: int = timestamp
+        self.system_data: SystemDataEntry = system_data_entry
+
+
+class SystemEntry:
+    def __init__(self, database_id: str, system_data: List[SystemData]):
+        self.database_id: str = database_id
+        self.system_data: List[SystemData] = system_data
--- tests/api/monitor/test_monitor_model.py
+++ tests/api/monitor/test_monitor_model.py
@@ -2,16 +2,21 @@ from hyrisecockpit.api.app.monitor.model import (
     FailedTaskEntry,
     FailedTasks,
     TimeInterval,
+    Cpu,
+    Memory,
+    SystemDataEntry,
+    SystemData,
+    SystemEntry,
 )
 
 
 class TestFailedTasksModel:
     def test_creates_failed_task_entry(self):
-        timestamp = 123456789
-        worker_id = "worker_01"
-        task = "select * from foo;"
-        error = "Table with name foo does not exist."
-        failed_task_model = FailedTaskEntry(
+        timestamp: int = 123456789
+        worker_id: str = "worker_01"
+        task: str = "select * from foo;"
+        error: str = "Table with name foo does not exist."
+        failed_task_model: FailedTaskEntry = FailedTaskEntry(
             timestamp=timestamp,
             worker_id=worker_id,
             task=task,
@@ -23,28 +28,28 @@ class TestFailedTasksModel:
         assert error == failed_task_model.error
 
     def test_creates_failed_tasks(self):
-        database_id = "database_id"
-        timestamp_failed_one = 1
-        worker_id_failed_one = "worker_01"
-        task_failed_one = "select * from foo;"
-        error_failed_one = "Table with name foo does not exist."
-        timestamp_failed_two = 2
-        worker_id_failed_two = "worker_02"
-        task_failed_two = "select * from foo;"
-        error_failed_two = "Table with name foo does not exist."
-        failed_task_model_one = FailedTaskEntry(
+        database_id: str = "database_id"
+        timestamp_failed_one: int = 1
+        worker_id_failed_one: str = "worker_01"
+        task_failed_one: str = "select * from foo;"
+        error_failed_one: str = "Table with name foo does not exist."
+        timestamp_failed_two: int = 2
+        worker_id_failed_two: str = "worker_02"
+        task_failed_two: str = "select * from foo;"
+        error_failed_two: str = "Table with name foo does not exist."
+        failed_task_model_one: FailedTaskEntry = FailedTaskEntry(
             timestamp=timestamp_failed_one,
             worker_id=worker_id_failed_one,
             task=task_failed_one,
             error=error_failed_one,
         )
-        failed_task_model_two = FailedTaskEntry(
+        failed_task_model_two: FailedTaskEntry = FailedTaskEntry(
             timestamp=timestamp_failed_two,
             worker_id=worker_id_failed_two,
             task=task_failed_two,
             error=error_failed_two,
         )
-        failed_task_model = FailedTasks(
+        failed_task_model: FailedTasks = FailedTasks(
             database_id=database_id,
             failed_task_entries=[failed_task_model_one, failed_task_model_two],
         )
@@ -63,3 +68,84 @@ class TestTimeIntervalModel:
             endts=2,
             precision=1,
         )
+
+
+class TestSystemModel:
+    def test_creates_cpu_model(self) -> None:
+        cpu_system_usage: float = 0.42
+        cpu_process_usage: float = 0.12
+        cpu_count: int = 32
+
+        cpu_model: Cpu = Cpu(
+            cpu_system_usage=cpu_system_usage,
+            cpu_process_usage=cpu_process_usage,
+            cpu_count=cpu_count,
+        )
+
+        assert cpu_model.cpu_system_usage == cpu_system_usage
+        assert cpu_model.cpu_process_usage == cpu_process_usage
+        assert cpu_model.cpu_count == cpu_count
+
+    def test_creates_memory_model(self) -> None:
+        free: int = 1234
+        available: int = 6789
+        total: int = 90000
+        percent: float = 10.42
+
+        memory_model: Memory = Memory(
+            free=free, available=available, total=total, percent=percent
+        )
+
+        assert memory_model.free == free
+        assert memory_model.available == available
+        assert memory_model.total == total
+        assert memory_model.percent == percent
+
+    def test_creates_system_data_entry_model(self) -> None:
+        cpu_model = Cpu(cpu_system_usage=0.42, cpu_process_usage=0.42, cpu_count=16)
+        memory_model = Memory(free=10, available=10, total=20, percent=10.2)
+        database_threads: int = 10
+
+        system_data_entry_model: SystemDataEntry = SystemDataEntry(
+            cpu=cpu_model, memory=memory_model, database_threads=database_threads
+        )
+
+        assert system_data_entry_model.cpu == cpu_model
+        assert system_data_entry_model.memory == memory_model
+        assert system_data_entry_model.database_threads == database_threads
+
+    def test_creates_system_data(self) -> None:
+        cpu_model = Cpu(cpu_system_usage=0.42, cpu_process_usage=0.42, cpu_count=16)
+        memory_model = Memory(free=10, available=10, total=20, percent=10.2)
+        database_threads: int = 10
+        system_data_entry_model: SystemDataEntry = SystemDataEntry(
+            cpu=cpu_model, memory=memory_model, database_threads=database_threads
+        )
+        timestamp: int = 42
+
+        sytem_data_model: SystemData = SystemData(
+            timestamp=timestamp, system_data_entry=system_data_entry_model
+        )
+
+        assert sytem_data_model.timestamp == timestamp
+        assert sytem_data_model.system_data == system_data_entry_model
+
+    def test_creates_system_entry(self) -> None:
+        cpu_model = Cpu(cpu_system_usage=0.42, cpu_process_usage=0.42, cpu_count=16)
+        memory_model = Memory(free=10, available=10, total=20, percent=10.2)
+        database_threads: int = 10
+        system_data_entry_model: SystemDataEntry = SystemDataEntry(
+            cpu=cpu_model, memory=memory_model, database_threads=database_threads
+        )
+        timestamp: int = 42
+        sytem_data_model: SystemData = SystemData(
+            timestamp=timestamp, system_data_entry=system_data_entry_model
+        )
+        database_id: str = "database_one"
+
+        system_entry_model: SystemEntry = SystemEntry(
+            database_id=database_id, system_data=[sytem_data_model]
+        )
+
+        assert system_entry_model.database_id == database_id
+        assert system_entry_model.system_data[0] == sytem_data_model
