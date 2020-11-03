434fd9a1a4cd7f50766c9bd03a205e588c4c6bb6
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-03 18:42:03 +0100

    Adjust memory footprint mock

--- hyrisecockpit/frontend/tests/e2e/setup/mocks.ts
+++ hyrisecockpit/frontend/tests/e2e/setup/mocks.ts
@@ -121,7 +121,7 @@ export function useMocks(
     responseMocks.memory_footprint = fakeDataByIdsWithTimestamps(
       mockedIds.databases,
       "memory_footprint",
-      fakeNumberData
+      ( ) => 0.0,
     );
     responseMocks.latency = fakeDataByIdsWithTimestamps(
       mockedIds.databases,
